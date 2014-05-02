196c92e18786cd52e17675657cb26fea0750b6b2
Richard Marriott <richard@xoaam.com>
2014-05-02 11:40:43 +0100

    Updated employer web to same as candidate with ioc etc.

--- src/SFA.Apprenticeships.Web.Candidate/App_Start/WebApiConfig.cs
+++ src/SFA.Apprenticeships.Web.Candidate/App_Start/WebApiConfig.cs
@@ -6,11 +6,6 @@ namespace SFA.Apprenticeships.Web.Candidate
     {
         public static void Register(HttpConfiguration config)
         {         
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional });
-
             config.MapHttpAttributeRoutes();
         }
     }
--- src/SFA.Apprenticeships.Web.Employer/Global.asax.cs
+++ src/SFA.Apprenticeships.Web.Employer/Global.asax.cs
@@ -1,4 +1,10 @@
-﻿namespace SFA.Apprenticeships.Web.Employer
+﻿using System.Web.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Optimization;
+using SFA.Apprenticeships.Web.Common.IoC.DependencyResolution;
+
+namespace SFA.Apprenticeships.Web.Employer
 {
     using System.Web.Mvc;
     using System.Web.Routing;
@@ -7,12 +13,32 @@
     {
         protected void Application_Start()
         {
+            const string applicationName = "SFA.Apprenticeships.Web.Employer";
+
+            // Change the Application Name in runtime.
+            var runtimeInfo = typeof(HttpRuntime).GetField("_theRuntime", BindingFlags.Static | BindingFlags.NonPublic);
+
+            if (runtimeInfo != null)
+            {
+                var theRuntime = (HttpRuntime)runtimeInfo.GetValue(null);
+                var appNameInfo = typeof(HttpRuntime).GetField("_appDomainAppId", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (appNameInfo != null)
+                {
+                    appNameInfo.SetValue(theRuntime, applicationName);
+                }
+            }
+
+
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
+
+            ControllerBuilder.Current.SetControllerFactory(new NasControllerFactory());
         }
     }
 }
