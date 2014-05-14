ed0ab82be76ab5b6aa3f9e6d9c3912d0e9969377
kristerbone <krister.bone@valtech.co.uk>
2014-05-14 17:52:20 +0100

    Added script to copy secure config to consistent location for tests

---
+++ setup/teamCityCopySecureConfig.ps1
@@ -0,0 +1,21 @@
+param(
+    [string]$checkoutRoot
+)
+
+if(!(Test-Path -Path "c:\Projects" )){
+    New-Item -ItemType directory -Path "C:\Projects"
+}
+
+if(!(Test-Path -Path "c:\Projects\SFA" )){
+    New-Item -ItemType directory -Path "C:\Projects\SFA"
+}
+
+if(!(Test-Path -Path "c:\Projects\SFA\Configuration" )){
+    New-Item -ItemType directory -Path "C:\Projects\SFA\Configuration"
+}
+
+Remove-Item C:\Projects\SFA\Configuration\* -recurse
+
+$secureSourcePath = "$checkoutRoot\Configuration\**"
+
+Copy-Item $secureSourcePath "C:\Projects\SFA\Configuration"
