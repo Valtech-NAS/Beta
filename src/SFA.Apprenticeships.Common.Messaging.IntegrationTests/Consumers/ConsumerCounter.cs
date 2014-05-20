5ed3df524d58034327e21072b39eeffd923d563e
kristerbone <krister.bone@valtech.co.uk>
2014-05-20 11:14:23 +0100

    Added integration test that tests both consume and consume async

---
+++ src/SFA.Apprenticeships.Common.Messaging.IntegrationTests/Consumers/ConsumerCounter.cs
@@ -0,0 +1,18 @@
+﻿namespace SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers
+{
+    public static class ConsumerCounter
+    {
+        static int _counter = 0;
+        static readonly object _lock = new object();
+
+        public static void IncrementCounter()
+        {
+            lock (_lock)
+            {
+                _counter++;
+            }
+        }
+
+        public static int Counter { get { return _counter; } }
+    }
+}
