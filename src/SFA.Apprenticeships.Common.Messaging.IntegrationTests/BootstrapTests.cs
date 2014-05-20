4a37706dfca0060ac3ac95aab1fee949dbfc8a61
kristerbone <krister.bone@valtech.co.uk>
2014-05-20 09:13:11 +0100

    Auto binding test added

--- src/SFA.Apprenticeships.Common.Messaging.IntegrationTests/BootstrapTests.cs
+++ src/SFA.Apprenticeships.Common.Messaging.IntegrationTests/BootstrapTests.cs
@@ -1,55 +1,15 @@
 ﻿namespace SFA.Apprenticeships.Common.Messaging.IntegrationTests
 {
-    using System;
     using System.Linq;
     using System.Reflection;
-    using EasyNetQ.Management.Client;
-    using EasyNetQ.Management.Client.Model;
     using FluentAssertions;
     using NUnit.Framework;
-    using SFA.Apprenticeships.Common.Configuration.Messaging;
 
     [TestFixture]
-    class BootstrapTests
+    public class BootstrapTests : RabbitSetUp
     {
-        private const string ExchangeName = "SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers.TestMessage:SFA.Apprenticeships.Common.Messaging.IntegrationTests";
-        private const string QueueNamre_Sync = "SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers.TestMessage:SFA.Apprenticeships.Common.Messaging.IntegrationTests_TestMessageConsumerSync";
-        private const string QueueNamre_Async = "SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers.TestMessage:SFA.Apprenticeships.Common.Messaging.IntegrationTests_TestMessageConsumerAsync";
-
-        private IManagementClient managementClient;
-
-        [SetUp]
-        public void Setup()
-        {
-            var rabitConfig = RabbitMQConfigurationSection.Instance;
-            managementClient = new ManagementClient(string.Format("http://{0}", rabitConfig.HostName), rabitConfig.UserName, rabitConfig.Password);
-            TearDown();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            var notAsyncQueue = GetQueue(QueueNamre_Sync);
-            if (notAsyncQueue != null)
-            {
-                managementClient.DeleteQueue(notAsyncQueue);
-            }
-
-            var asyncQueue = GetQueue(QueueNamre_Async);
-            if (asyncQueue != null)
-            {
-                managementClient.DeleteQueue(asyncQueue);
-            }
-
-            var exchange = GetExchange(ExchangeName);
-            if (exchange != null)
-            {
-                managementClient.DeleteExchange(exchange);
-            }
-        }
-
         [Test]
-        public void AutoBindBindsSubscriptions()
+        public void AutoBindsSubscriptions()
         {
             var bus = Transport.CreateBus();
             var bs = new Bootstrapper(bus);
@@ -82,20 +42,6 @@
             asyncBindingToSelf.Should().NotBeNull();
             var asyncBindingToExchange = asyncBindings.SingleOrDefault(b => b.RoutingKey == "#");
             asyncBindingToExchange.Should().NotBeNull();
-        }
-
-        private Queue GetQueue(string queueName)
-        {
-            var queues = managementClient.GetQueues();
-            var queue = queues.SingleOrDefault(q => q.Name == queueName);
-            return queue;
-        }
-
-        private Exchange GetExchange(string exchangeName)
-        {
-            var exchanges = managementClient.GetExchanges();
-            var exchange = exchanges.SingleOrDefault(q => q.Name == exchangeName);
-            return exchange;
         }
     }
 }
---
+++ src/SFA.Apprenticeships.Common.Messaging.IntegrationTests/RabbitSetUp.cs
@@ -0,0 +1,61 @@
+﻿namespace SFA.Apprenticeships.Common.Messaging.IntegrationTests
+{
+    using System.Linq;
+    using EasyNetQ.Management.Client;
+    using EasyNetQ.Management.Client.Model;
+    using NUnit.Framework;
+    using SFA.Apprenticeships.Common.Configuration.Messaging;
+
+    [SetUpFixture]
+    public class RabbitSetUp
+    {
+        protected const string ExchangeName = "SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers.TestMessage:SFA.Apprenticeships.Common.Messaging.IntegrationTests";
+        protected const string QueueNamre_Sync = "SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers.TestMessage:SFA.Apprenticeships.Common.Messaging.IntegrationTests_TestMessageConsumerSync";
+        protected const string QueueNamre_Async = "SFA.Apprenticeships.Common.Messaging.IntegrationTests.Consumers.TestMessage:SFA.Apprenticeships.Common.Messaging.IntegrationTests_TestMessageConsumerAsync";
+
+        protected IManagementClient managementClient;
+
+        [SetUp]
+        public virtual void BeforeAllTests()
+        {
+            Common.IoC.IoC.Initialize();
+            var rabitConfig = RabbitMQConfigurationSection.Instance;
+            managementClient = new ManagementClient(string.Format("http://{0}", rabitConfig.HostName), rabitConfig.UserName, rabitConfig.Password);
+        }
+
+        [TearDown]
+        public virtual void AfterAllTests()
+        {
+            var notAsyncQueue = GetQueue(QueueNamre_Sync);
+            if (notAsyncQueue != null)
+            {
+                managementClient.DeleteQueue(notAsyncQueue);
+            }
+
+            var asyncQueue = GetQueue(QueueNamre_Async);
+            if (asyncQueue != null)
+            {
+                managementClient.DeleteQueue(asyncQueue);
+            }
+
+            var exchange = GetExchange(ExchangeName);
+            if (exchange != null)
+            {
+                managementClient.DeleteExchange(exchange);
+            }
+        }
+        protected Queue GetQueue(string queueName)
+        {
+            var queues = managementClient.GetQueues();
+            var queue = queues.SingleOrDefault(q => q.Name == queueName);
+            return queue;
+        }
+
+        protected Exchange GetExchange(string exchangeName)
+        {
+            var exchanges = managementClient.GetExchanges();
+            var exchange = exchanges.SingleOrDefault(q => q.Name == exchangeName);
+            return exchange;
+        }
+    }
+}
