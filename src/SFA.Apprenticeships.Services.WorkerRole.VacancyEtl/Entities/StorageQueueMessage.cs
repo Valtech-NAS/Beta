31004362baebddeba091d4bd09c47072a3c95d43
kristerbone <krister.bone@valtech.co.uk>
2014-05-21 10:38:27 +0100

    Checkin before refactoring to interface cloud queue and cloud config to allow unit testing.

---
+++ src/SFA.Apprenticeships.Services.WorkerRole.VacancyEtl/Entities/StorageQueueMessage.cs
@@ -0,0 +1,46 @@
+﻿namespace SFA.Apprenticeships.Services.WorkerRole.VacancyEtl.Entities
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// storage queue message
+    /// </summary>
+    [DataContract]
+    public sealed class StorageQueueMessage
+    {
+        /// <summary>
+        /// Gets or sets the ETag 
+        /// </summary>
+        public string ExecutionTag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Client Request ID
+        /// </summary>
+        public string ClientRequestId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Expected executionTime
+        /// </summary>
+        public string ExpectedExecutionTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Scheduler Job ID
+        /// </summary>
+        public string SchedulerJobId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Scheduler JobCollection ID
+        /// </summary>
+        public string SchedulerJobCollectionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Region
+        /// </summary>
+        public string Region { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Message
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
