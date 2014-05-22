dfc5e717ec71290411ed19e947e98ef429fd8551
Richard Marriott <richard@xoaam.com>
2014-05-22 13:13:57 +0100

    Removed caching decorator from legacy data provider

--- src/SFA.Apprenticeships.Web.Common.Tests/Providers/LegacyReferenceDataProviderTests.cs
+++ src/SFA.Apprenticeships.Web.Common.Tests/Providers/LegacyReferenceDataProviderTests.cs
@@ -5,35 +5,20 @@
     using FluentAssertions;
     using Moq;
     using NUnit.Framework;
-    using SFA.Apprenticeships.Common.Caching;
-
-    using SFA.Apprenticeships.Web.Common.Models.Common;
-    using SFA.Apprenticeships.Web.Common.Providers;
-    using Model = SFA.Apprenticeships.Common.Entities.ReferenceData;
     using SFA.Apprenticeships.Common.Interfaces.Enums.ReferenceDataService;
     using SFA.Apprenticeships.Common.Interfaces.ReferenceData;
+    using SFA.Apprenticeships.Web.Common.Providers;
+    using Model = SFA.Apprenticeships.Common.Entities.ReferenceData;
 
     [TestFixture]
     public class LegacyReferenceDataProviderTests
     {
         private Mock<IReferenceDataService> _service;
-        private Mock<ICacheClient> _cache;
-        private IList<ReferenceDataViewModel> _frameworks;
-        private IList<ReferenceDataViewModel> _counties;
-        private IList<ReferenceDataViewModel> _errorCodes;
-        private IList<ReferenceDataViewModel> _localAuthorities;
-        private IList<ReferenceDataViewModel> _regions;
 
         [TestFixtureSetUp]
         public void FixtureSetup()
         {
             _service = new Mock<IReferenceDataService>();
-
-            _frameworks = new List<ReferenceDataViewModel> { new ReferenceDataViewModel { Id = "Framework", Description = "Framework.1" } };
-            _counties = new List<ReferenceDataViewModel> { new ReferenceDataViewModel { Id = "County", Description = "County.1" } };
-            _errorCodes = new List<ReferenceDataViewModel> { new ReferenceDataViewModel { Id = "ErrorCode", Description = "ErrorCode.1" } };
-            _localAuthorities = new List<ReferenceDataViewModel> { new ReferenceDataViewModel { Id = "LocalAuthority", Description = "LocalAuthority.1" } };
-            _regions = new List<ReferenceDataViewModel> { new ReferenceDataViewModel { Id = "Region", Description = "Region.1" } };
 
             _service.Setup(x => x.GetReferenceData(LegacyReferenceDataType.Framework)).Returns(new List<ILegacyReferenceData>{new Model.Framework{ Id = "Framework", Description = "Framework.1" }});
             _service.Setup(x => x.GetReferenceData(LegacyReferenceDataType.County)).Returns(new List<ILegacyReferenceData> { new Model.Framework { Id = "County", Description = "County.1" } });
@@ -45,7 +30,6 @@
         [SetUp]
         public void TestSetup()
         {
-            _cache = new Mock<ICacheClient>();
             _service.ResetCalls();
         }
 
@@ -64,58 +48,7 @@
             viewModel.Count().Should().Be(1);
             viewModel.First(x => x.Id == type.ToString()).Description.Should().Be(string.Format("{0}.1", type));
 
-            _cache.Verify(
-                x => x.Put(It.IsAny<LegacyDataProviderCacheKeyEntry>(), It.IsAny<IList<ILegacyReferenceData>>(), It.IsAny<LegacyReferenceDataType>()), Times.Never);
-
             _service.Verify(x => x.GetReferenceData(type), Times.Once);
-        }
-
-        [TestCase(LegacyReferenceDataType.County)]
-        [TestCase(LegacyReferenceDataType.ErrorCode)]
-        [TestCase(LegacyReferenceDataType.Framework)]
-        [TestCase(LegacyReferenceDataType.LocalAuthority)]
-        [TestCase(LegacyReferenceDataType.Region)]
-        public void DoesGetReturnLegacyReferenceDataWithEmptyCache(LegacyReferenceDataType type)
-        {
-            var provider = new CacheLegacyReferenceDataProvider(_cache.Object, new LegacyReferenceDataProvider(_service.Object));
-
-            var viewModel = provider.Get(type);
-
-            viewModel.Should().NotBeNull();
-            viewModel.Count().Should().Be(1);
-            viewModel.First(x => x.Id == type.ToString()).Description.Should().Be(string.Format("{0}.1", type));
-
-            _cache.Verify(
-                x => x.Put(It.IsAny<LegacyDataProviderCacheKeyEntry>(), It.IsAny<IEnumerable<ReferenceDataViewModel>>(), It.IsAny<LegacyReferenceDataType>()), Times.Once);
-
-            _service.Verify(x => x.GetReferenceData(type), Times.Once);
-        }
-
-        [TestCase(LegacyReferenceDataType.County)]
-        [TestCase(LegacyReferenceDataType.ErrorCode)]
-        [TestCase(LegacyReferenceDataType.Framework)]
-        [TestCase(LegacyReferenceDataType.LocalAuthority)]
-        [TestCase(LegacyReferenceDataType.Region)]
-        public void DoesGetReturnLegacyReferenceDataWithCache(LegacyReferenceDataType type)
-        {
-            _cache.Setup(x => x.Get<IEnumerable<ReferenceDataViewModel>>(new LegacyDataProviderCacheKeyEntry().Key("Framework"))).Returns(_frameworks);
-            _cache.Setup(x => x.Get<IEnumerable<ReferenceDataViewModel>>(new LegacyDataProviderCacheKeyEntry().Key("County"))).Returns(_counties);
-            _cache.Setup(x => x.Get<IEnumerable<ReferenceDataViewModel>>(new LegacyDataProviderCacheKeyEntry().Key("ErrorCode"))).Returns(_errorCodes);
-            _cache.Setup(x => x.Get<IEnumerable<ReferenceDataViewModel>>(new LegacyDataProviderCacheKeyEntry().Key("Region"))).Returns(_regions);
-            _cache.Setup(x => x.Get<IEnumerable<ReferenceDataViewModel>>(new LegacyDataProviderCacheKeyEntry().Key("LocalAuthority"))).Returns(_localAuthorities);
-
-            var provider = new CacheLegacyReferenceDataProvider(_cache.Object, new LegacyReferenceDataProvider(_service.Object));
-
-            var viewModel = provider.Get(type);
-
-            viewModel.Should().NotBeNull();
-            viewModel.Count().Should().Be(1);
-            viewModel.First(x => x.Id == type.ToString()).Description.Should().Be(string.Format("{0}.1", type));
-
-            _cache.Verify(
-                x => x.Put(It.IsAny<LegacyDataProviderCacheKeyEntry>(), It.IsAny<IList<ILegacyReferenceData>>(), It.IsAny<LegacyReferenceDataType>()), Times.Never);
-
-            _service.Verify(x => x.GetReferenceData(type), Times.Never);
         }
     }
 }
--- src/SFA.Apprenticeships.Web.Common/IoC/WebCommonRegistry.cs
+++ src/SFA.Apprenticeships.Web.Common/IoC/WebCommonRegistry.cs
@@ -1,6 +1,5 @@
 ﻿namespace SFA.Apprenticeships.Web.Common.IoC
 {
-    using SFA.Apprenticeships.Common.Caching;
     using SFA.Apprenticeships.Services.Common.ActiveDirectory;
     using SFA.Apprenticeships.Web.Common.Providers;
     using StructureMap.Configuration.DSL;
@@ -10,10 +9,7 @@
         public WebCommonRegistry()
         {
             For<IActiveDirectoryConfiguration>().Singleton().Use(ActiveDirectoryConfigurationSection.Instance);
-
-            // Use cache decorated provider.
-            For<IReferenceDataProvider>().Use<LegacyReferenceDataProvider>()
-                .DecorateWith((ctx, provider) => new CacheLegacyReferenceDataProvider(ctx.GetInstance<ICacheClient>(), provider));
+            For<IReferenceDataProvider>().Use<LegacyReferenceDataProvider>();
         }
     }
 }
