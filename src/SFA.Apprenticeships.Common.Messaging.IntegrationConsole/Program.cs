88aa76c4cfc29f74d7d19dc379a0d8a62b0f4826
Richard Marriott <richard@xoaam.com>
2014-05-22 14:58:24 +0100

    Added azure control config for console test app.

--- src/SFA.Apprenticeships.Common.Messaging.IntegrationConsole/Program.cs
+++ src/SFA.Apprenticeships.Common.Messaging.IntegrationConsole/Program.cs
@@ -3,12 +3,11 @@ namespace SFA.Apprenticeships.Common.Messaging.IntegrationConsole
 {
     using System;
     using System.Threading;
-    using EasyNetQ;
+    using SFA.Apprenticeships.Common.Configuration;
     using SFA.Apprenticeships.Common.Configuration.Azure;
     using SFA.Apprenticeships.Common.Entities.Vacancy;
     using SFA.Apprenticeships.Common.Interfaces.Elasticsearch;
     using SFA.Apprenticeships.Common.Messaging.Azure;
-    using SFA.Apprenticeships.Common.Messaging.Interfaces;
     using SFA.Apprenticeships.Services.Legacy.Vacancy.Abstract;
     using SFA.Apprenticeships.Services.VacancyEtl.Consumers;
     using SFA.Apprenticeships.Services.VacancyEtl.Load;
@@ -23,11 +22,11 @@ namespace SFA.Apprenticeships.Common.Messaging.IntegrationConsole
             IoC.IoC.Initialize();
             ElasticsearchLoad<VacancySummary>.Setup(ObjectFactory.GetInstance<IElasticsearchService>());
             var bus = RabbitQueue.Setup();
-            var client = new AzureCloudClient(new AzureCloudConfig());
+            var client = new AzureCloudClient(new AzureConsoleConfig(ObjectFactory.GetInstance<IConfigurationManager>()));
 
             var vacancySchedulerConsumer = new VacancySchedulerConsumer(
                                                 bus,
-                                                ObjectFactory.GetInstance<IAzureCloudClient>(),
+                                                client,
                                                 ObjectFactory.GetInstance<IVacancySummaryService>());
 
 
