e0137c00fc27d9a75b684a64c0011a0069f78afb
Richard Marriott <richard@xoaam.com>
2014-05-22 15:11:20 +0100

    Console integration test

--- src/SFA.Apprenticeships.Common.Messaging.IntegrationConsole/Program.cs
+++ src/SFA.Apprenticeships.Common.Messaging.IntegrationConsole/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace SFA.Apprenticeships.Common.Messaging.IntegrationConsole
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Threading;
+    using System.Xml.Serialization;
+    using Microsoft.WindowsAzure.Storage.Queue;
     using SFA.Apprenticeships.Common.Configuration;
     using SFA.Apprenticeships.Common.Configuration.Azure;
     using SFA.Apprenticeships.Common.Entities.Vacancy;
@@ -9,6 +13,7 @@
     using SFA.Apprenticeships.Common.Messaging.Azure;
     using SFA.Apprenticeships.Services.Legacy.Vacancy.Abstract;
     using SFA.Apprenticeships.Services.VacancyEtl.Consumers;
+    using SFA.Apprenticeships.Services.VacancyEtl.Entities;
     using SFA.Apprenticeships.Services.VacancyEtl.Load;
     using SFA.Apprenticeships.Services.VacancyEtl.Queue;
     using StructureMap;
@@ -32,12 +37,44 @@
             Console.WriteLine("Enter any key to quite");
             Console.WriteLine("---------------------------------------------------------------");
 
+            GetAzureScheduledMessagesQueue(1);
+
             while (!Console.KeyAvailable)
             {
                 var task = vacancySchedulerConsumer.CheckScheduleQueue();
                 task.Wait();
                 Thread.Sleep(5000);
             }
+        }
+
+        private static Queue<CloudQueueMessage> GetAzureScheduledMessagesQueue(int count)
+        {
+            var queue = new Queue<CloudQueueMessage>();
+            var serializer = new XmlSerializer(typeof(StorageQueueMessage));
+
+            for (int i = count; i > 0; i--)
+            {
+                var storageScheduleMessage = new StorageQueueMessage
+                {
+                    ClientRequestId = Guid.NewGuid().ToString(),
+                    SchedulerJobId = i.ToString()
+                };
+
+                string message;
+                using (var ms = new MemoryStream())
+                {
+                    serializer.Serialize(ms, storageScheduleMessage);
+                    ms.Position = 0;
+                    var sr = new StreamReader(ms);
+                    message = sr.ReadToEnd();
+                }
+
+                var cloudMessage = new CloudQueueMessage(message);
+                queue.Enqueue(cloudMessage);
+            }
+
+            queue.Enqueue(null);
+            return queue;
         }
     }
 }
