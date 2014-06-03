d028c3ea7e835a8f683024757a23620d0d78cd69
kristerbone <krister.bone@valtech.co.uk>
2014-06-03 19:13:54 +0100

    Fixing config tests

--- src/SFA.Apprenticeships.Infrastructure.RabbitMq.Tests/Configuration/RabbitMQConfigurationTests.cs
+++ src/SFA.Apprenticeships.Infrastructure.RabbitMq.Tests/Configuration/RabbitMQConfigurationTests.cs
@@ -12,18 +12,20 @@
         {
             var rabbitHostsConfig = RabbitMqHostsConfiguration.Instance;
 
-            rabbitHostsConfig.DefaultHost.Should().Be("One");
+            rabbitHostsConfig.DefaultHost.Should().Be("Test");
             
-            var testconfig1 = rabbitHostsConfig.RabbitHosts["One"];
+            var testconfig1 = rabbitHostsConfig.RabbitHosts["Test"];
 
-            testconfig1.VirtualHost.Should().Be("testvh1");
-            testconfig1.UserName.Should().Be("testun1");
-            testconfig1.Password.Should().Be("testpw1");
-            testconfig1.Port.Should().Be(1234);
-            testconfig1.HostName.Should().Be("testhn1");
-            testconfig1.Durable.Should().Be(false);
-            testconfig1.HeartBeatSeconds.Should().Be(4321);
-            testconfig1.OutputEasyNetQLogsToNLogInternal.Should().Be(true);
+            //Conatins real config so don't test these values.
+            testconfig1.VirtualHost.Should().NotBeEmpty();
+            testconfig1.UserName.Should().NotBeEmpty();
+            testconfig1.Password.Should().NotBeEmpty();
+            testconfig1.Port.Should().BeGreaterThan(0);
+            testconfig1.HostName.Should().NotBeEmpty();
+            testconfig1.Durable.Should().Be(true);
+            testconfig1.HeartBeatSeconds.Should().BeGreaterThan(0);
+            testconfig1.OutputEasyNetQLogsToNLogInternal.Should().Be(false);
+            testconfig1.PreFetchCount.Should().Be(1);
 
             var testconfig2 = RabbitMqHostsConfiguration.Instance.RabbitHosts["Two"];
 
@@ -32,9 +34,10 @@
             testconfig2.Password.Should().Be("testpw2");
             testconfig2.Port.Should().Be(1235);
             testconfig2.HostName.Should().Be("testhn2");
-            testconfig2.Durable.Should().Be(true);
+            testconfig2.Durable.Should().Be(false);
             testconfig2.HeartBeatSeconds.Should().Be(4322);
-            testconfig2.OutputEasyNetQLogsToNLogInternal.Should().Be(false);
+            testconfig2.OutputEasyNetQLogsToNLogInternal.Should().Be(true);
+            testconfig1.PreFetchCount.Should().Be(1);
         }
     }
 }
