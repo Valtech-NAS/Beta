9543fa04cd2eb137d224f6a352dec9928c38144f
kristerbone <krister.bone@valtech.co.uk>
2014-06-04 14:18:26 +0100

    Updated/added shared assembly info for all packages

--- src/SFA.Apprenticeships.Infrastructure.Elasticsearch/Properties/AssemblyInfo.cs
+++ src/SFA.Apprenticeships.Infrastructure.Elasticsearch/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
 ﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SFA.Apprenticeships.Repository.Elasticsearch")]
+[assembly: AssemblyTitle("SFA.Apprenticeships.Infrastructure.Elasticsearch")]
 [assembly: AssemblyDescription("")]
