ed4919a207e493762e2f442c0f045da10bed5c8a
kristerbone <krister.bone@valtech.co.uk>
2014-06-09 14:29:33 +0100

    Updated dropdowns to use MVC SelectList

--- src/SFA.Apprenticeships.Web.Candidate/Controllers/VacancySearchController.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Controllers/VacancySearchController.cs
@@ -1,7 +1,9 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.Controllers
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
+    using SFA.Apprenticeships.Domain.Entities.Vacancy;
     using SFA.Apprenticeships.Web.Candidate.Providers;
     using SFA.Apprenticeships.Web.Candidate.ViewModels.VacancySearch;
 
@@ -17,22 +19,50 @@
         // GET: VacancySearch
         public ActionResult Index()
         {
+            PopulateDistances();
             return View();
         }
 
         // GET: VacancySearch/Results
         public ActionResult Results(VacancySearchViewModel searchViewModel)
         {
+            PopulateDistances(searchViewModel.WithinDistance);
             var locations = _searchProvider.FindLocation(searchViewModel.Location);
 
-            if (locations.Count() == 1)
+            if (locations != null && locations.Count() == 1)
             {
                 var results = _searchProvider.FindVacancies(locations.First(), searchViewModel.WithinDistance);
                 results.VacancySearch = searchViewModel;
                 return View(results);    
             }
 
-            return View();
+            // Test code, to be removed
+            var vacancySearchResponseViewModel = new VacancySearchResponseViewModel();
+            vacancySearchResponseViewModel.Vacancies = new List<VacancySummary>();
+            vacancySearchResponseViewModel.VacancySearch = searchViewModel;
+
+            return View(vacancySearchResponseViewModel);
+        }
+
+        private void PopulateDistances(int selectedValue = 2)
+        {
+            var distances = new SelectList(
+                new[] 
+                        {
+                            new { WithinDistance = 2, Name = "This area only" },
+                            new { WithinDistance = 5, Name = "5 miles" },
+                            new { WithinDistance = 10, Name = "10 miles" },
+                            new { WithinDistance = 15, Name = "15 miles" },
+                            new { WithinDistance = 20, Name = "20 miles" },
+                            new { WithinDistance = 30, Name = "30 miles" },
+                            new { WithinDistance = 40, Name = "40 miles" },
+                            new { WithinDistance = 0, Name = "Nationwide" },
+                        },
+                "WithinDistance",
+                "Name",
+                selectedValue
+            );
+            ViewBag.Distances = distances;
         }
     }
 }
