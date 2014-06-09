19c2793583030e588da97b41c915438be262b95a
Richard Marriott <richard@xoaam.com>
2014-06-09 10:26:40 +0100

    Fixed break.

--- src/SFA.Apprenticeships.Web.Candidate/Providers/SearchProvider.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Providers/SearchProvider.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace SFA.Apprenticeships.Web.Candidate.Providers
 {
+    using System.Collections.Generic;
+    using SFA.Apprenticeships.Web.Candidate.ViewModels.VacancySearch;
+
     public class SearchProvider : ISearchProvider
     {
-        public IEnumerable<LookupLocation> FindLocation(string placeNameOrPostcode)
+        public IEnumerable<LocationViewModel> FindLocation(string placeNameOrPostcode)
         {
             throw new NotImplementedException();
         }
