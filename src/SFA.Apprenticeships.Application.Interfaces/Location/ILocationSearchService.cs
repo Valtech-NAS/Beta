24b81e0225956f8db63d88f31f20ec601fa1d2eb
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-06-09 11:47:58 +0100

    comment update

--- src/SFA.Apprenticeships.Application.Interfaces/Location/ILocationSearchService.cs
+++ src/SFA.Apprenticeships.Application.Interfaces/Location/ILocationSearchService.cs
@@ -7,7 +7,7 @@
     public interface ILocationSearchService
     {
         /// <summary>
-        /// returns locations that match the place name passed in
+        /// returns locations that match the place name or postcode passed in
         /// </summary>
         /// <param name="placeNameOrPostcode">place name (town, city, village, county, etc.) or postcode</param>
         /// <returns>0..* matching locations</returns>
