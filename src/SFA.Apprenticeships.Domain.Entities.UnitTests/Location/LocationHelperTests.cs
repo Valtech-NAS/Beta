d17623e26b34379f6c780b20091bd95473b31193
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-06-09 13:16:33 +0100

    merged

---
+++ src/SFA.Apprenticeships.Domain.Entities.UnitTests/Location/LocationHelperTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SFA.Apprenticeships.Domain.Entities.UnitTests.Location
+{
+    using System;
+    using NUnit.Framework;
+    using Entities.Location;
+
+    [TestFixture]
+    public class LocationHelperTests
+    {
+        [TestCase("CV12WT")]
+        [TestCase("cv12wt")]
+        [TestCase("CV1 2WT")]
+        [TestCase(" CV1 2WT ")]
+        public void ShouldBeIdentitiedAsPostcode(string postcode)
+        {
+            Assert.IsTrue(LocationHelper.IsPostcode(postcode));
+        }
+
+        [TestCase("London")]
+        [TestCase(" C V 1 2WT ")]
+        [TestCase("")]
+        [TestCase(default(string))]
+        public void ShouldNotBeIdentitiedAsPostcode(string postcode)
+        {
+            Assert.IsFalse(LocationHelper.IsPostcode(postcode));
+        }
+    }
+}
--- src/SFA.Apprenticeships.Domain.Entities/Location/LocationHelper.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Location/LocationHelper.cs
@@ -1,14 +1,20 @@
 ﻿namespace SFA.Apprenticeships.Domain.Entities.Location
 {
     using System;
+    using System.Text.RegularExpressions;
 
     public class LocationHelper
     {
-        const string PostcodeRegex = "TODO"; // http://regexlib.com/REDetails.aspx?regexp_id=2653
+        // adapted from http://regexlib.com/REDetails.aspx?regexp_id=2653
+        const string PostcodeRegex = @"(?<O>(?<d>[BEGLMNS]|A[BL]|B[ABDHLNRST]|C[ABFHMORTVW]|D[ADEGHLNTY]|E[HNX]|F[KY]|G[LUY]|H[ADGPRSUX]|I[GMPV]|JE|K[ATWY]|L[ADELNSU]|M[EKL]|N[EGNPRW]|O[LX]|P[AEHLOR]|R[GHM]|S[AEGKL-PRSTWY]|T[ADFNQRSW]|UB|W[ADFNRSV]|YO|ZE)(?<a>\d\d?)|(?<d>E)(?<a>\dW)|(?<d>EC)(?<a>\d[AMNPRVY0])|(?<d>N)(?<a>\dP)|(?<d>NW)(?<a>\dW)|(?<d>SE)(?<a>\dP)|(?<d>SW)(?<a>\d[AEHPVWXY])|(?<d>W)(?<a>1[0-4A-DFGHJKSTUW])|(?<d>W)(?<a>[2-9])|(?<d>WC)(?<a>[12][ABEHNRVX]))(\ {0,1})(?<I>(?<s>\d)(?<u>[ABD-HJLNP-UW-Z]{2}))";
 
         public static bool IsPostcode(string postcode)
         {
-            return true; //todo: use regex to see if the postcode passed in, need unit tests
+            if (string.IsNullOrWhiteSpace(postcode)) return false;
+
+            var formattedPostcode = postcode.Trim().ToUpperInvariant();
+
+            return Regex.IsMatch(formattedPostcode, PostcodeRegex);
         }
     }
 }
