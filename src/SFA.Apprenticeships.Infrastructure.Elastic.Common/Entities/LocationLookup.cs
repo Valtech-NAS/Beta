b5ed0d671fff3db3acf0b9a4b97c31cf0a8a2eb4
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-06-19 17:41:33 +0100

    removed redundant fields from location lookup entity

--- src/SFA.Apprenticeships.Infrastructure.Elastic.Common/Entities/LocationLookup.cs
+++ src/SFA.Apprenticeships.Infrastructure.Elastic.Common/Entities/LocationLookup.cs
@@ -3,7 +3,6 @@
     using System;
     using Nest;
 
-    //[ElasticType(Name = "location")]
     [ElasticType(Name = "locationdatas")]
     public class LocationLookup
     {
@@ -13,16 +12,10 @@
         [ElasticProperty(Name = "county", Type = FieldType.string_type, Store = true, Index = FieldIndexOption.analyzed)]
         public string County { get; set; }
 
-        [ElasticProperty(Name = "country", Type = FieldType.string_type, Store = true, Index = FieldIndexOption.not_analyzed)]
-        public string Country { get; set; }
-
         [ElasticProperty(Name = "longitude", Type = FieldType.double_type, Store = true, Index = FieldIndexOption.not_analyzed)]
         public double Longitude { get; set; }
 
         [ElasticProperty(Name = "latitude", Type = FieldType.double_type, Store = true, Index = FieldIndexOption.not_analyzed)]
         public double Latitude { get; set; }
-
-        [ElasticProperty(Name = "postcode", Type = FieldType.string_type, Store = true, Index = FieldIndexOption.analyzed)]
-        public string Postcode { get; set; }
     }
 }
