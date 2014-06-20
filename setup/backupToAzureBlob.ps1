4c25b7375e1ec92d6afdc01fdf051e0c1e70c6db
Richard Marriott <richard@xoaam.com>
2014-06-20 11:17:48 +0100

    Updated deploy ps for backup filename

--- setup/backupToAzureBlob.ps1
+++ setup/backupToAzureBlob.ps1
@@ -15,14 +15,12 @@ Write-Output "Running Azure Imports"
 Import-Module "C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\ServiceManagement\Azure\*.psd1"
 Import-AzurePublishSettingsFile "C:\TeamCity\Visual Studio Ultimate with MSDN-6-13-2014-credentials.publishsettings"
 
-Write-Host "Files will be uploaded to the storage account/Container"
-#create a storage context 
+Write-Host "Files will be uploaded to $storageAccountName\$storage"
 $context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storagekey
 
+Write-Host "Writing files" 
 $fqName = "$srcpath\ServiceConfiguration.Cloud.cscfg"
-Write-Host "Writing file $fqName" 
 Set-AzureStorageBlobContent -Blob "$environment\$sitename\$buildnumber\ServiceConfiguration.Cloud.cscfg" -Container "$storage" -File "$fqName" -Context $context -Force
 
-$fqName = "$srcpath\SFA.Apprenticeships.Web.Candidate.Azure.cspkg"
-Write-Host "Writing file $fqName" 
+$fqName = "$srcpath\$projectname.cspkg"
 Set-AzureStorageBlobContent -Blob "$environment\$sitename\$buildnumber\SFA.Apprenticeships.Web.Candidate.Azure.cspkg" -Container "$storage" -File "$fqName" -Context $context -Force
