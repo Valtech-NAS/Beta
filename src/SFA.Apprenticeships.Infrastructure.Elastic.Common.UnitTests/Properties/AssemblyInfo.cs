c37f30922d72e5a3a5e73756a427bed403e4f418
Richard Marriott <richard@xoaam.com>
2014-06-20 09:09:20 +0100

    Update all assemblyinfo file to move version numbers out of shared in order for teamcity to update them correctly.

--- src/SFA.Apprenticeships.Infrastructure.Elastic.Common.UnitTests/Properties/AssemblyInfo.cs
+++ src/SFA.Apprenticeships.Infrastructure.Elastic.Common.UnitTests/Properties/AssemblyInfo.cs
@@ -1,3 +1,12 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("SFA.Apprenticeships.Infrastructure.Elastic.Common.UnitTests")]
 [assembly: AssemblyDescription("")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
