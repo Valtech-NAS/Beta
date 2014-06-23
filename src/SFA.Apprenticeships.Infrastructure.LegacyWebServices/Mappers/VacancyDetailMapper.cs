7f1b85f2ffadb8f653c3319b79a21c0057b4bc07
kristerbone <krister.bone@valtech.co.uk>
2014-06-23 17:53:44 +0100

    Fixed mapping test - added fields missing from map

--- src/SFA.Apprenticeships.Infrastructure.LegacyWebServices/Mappers/VacancyDetailMapper.cs
+++ src/SFA.Apprenticeships.Infrastructure.LegacyWebServices/Mappers/VacancyDetailMapper.cs
@@ -13,6 +13,7 @@ namespace SFA.Apprenticeships.Infrastructure.LegacyWebServices.Mappers
                 .ForMember(d => d.Id, opt => opt.MapFrom(src => src.VacancyReference))
                 .ForMember(d => d.LocalAuthority, opt => opt.MapFrom(src => src.VacancyAddress.LocalAuthority))
                 .ForMember(d => d.Created, opt => opt.MapFrom(src => src.CreatedDateTime))
+                .ForMember(d => d.StartDate, opt => opt.MapFrom(src => src.PossibleStartDate))
                 .ForMember(d => d.Description, opt => opt.MapFrom(src => src.ShortDescription))
                 .ForMember(d => d.Framework, opt => opt.MapFrom(src => src.ApprenticeshipFramework))
                 .ForMember(d => d.VacancyAddress, opt => opt.ResolveUsing<VacancyDetailAddressResolver>().FromMember(src => src.VacancyAddress))
@@ -20,7 +21,13 @@ namespace SFA.Apprenticeships.Infrastructure.LegacyWebServices.Mappers
                 .ForMember(d => d.Title, opt => opt.MapFrom(src => src.VacancyTitle))
                 .ForMember(d => d.VacancyLocationType, opt => opt.ResolveUsing<VacancyLocationTypeResolver>().FromMember(src => src.VacancyLocationType))
                 .ForMember(d => d.VacancyType, opt => opt.ResolveUsing<VacancyTypeResolver>().FromMember(src => src.VacancyType))
-                .ForMember(d => d.WageType, opt => opt.MapFrom(src => src.WageType));
+                .ForMember(d => d.WageType, opt => opt.MapFrom(src => src.WageType))
+                .ForMember(d => d.WageDescription, opt => opt.MapFrom(src => src.WageText))
+                .ForMember(d => d.OtherInformation, opt => opt.MapFrom(src => src.OtherImportantInformation))
+                .ForMember(d => d.ProviderDescription, opt => opt.MapFrom(src => src.LearningProviderDesc))
+                .ForMember(d => d.Contact, opt => opt.MapFrom(src => src.ContactPerson))
+                .ForMember(d => d.ProviderSectorPassRate, opt => opt.MapFrom(src => src.LearningProviderSectorPassRate));
+;
         }
     }
 }
