0901364afb324741fa8cac221a07a09bb26e6ea8
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-01 12:44:13 +0100

    added UPRN to address entity

--- src/SFA.Apprenticeships.Domain.Entities/Locations/Address.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Locations/Address.cs
@@ -9,6 +9,7 @@ namespace SFA.Apprenticeships.Domain.Entities.Locations
         public string AddressLine3 { get; set; }
         public string AddressLine4 { get; set; }
         public string Postcode { get; set; }
+        public string Uprn { get; set; }
         public GeoPoint GeoPoint { get; set; }
     }
 }
