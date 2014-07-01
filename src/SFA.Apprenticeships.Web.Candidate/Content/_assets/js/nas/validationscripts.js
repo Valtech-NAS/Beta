8726f6c3dd0c39f9a67c43699350a9945dfa8dad
kristerbone <krister.bone@valtech.co.uk>
2014-07-01 13:48:45 +0100

    Validator updated to check for multiple forms + tidy up.

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/validationscripts.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/validationscripts.js
@@ -1,14 +1,17 @@
 ﻿$(document).ready(function() {
 
-    var settngs = $.data($('form')[0], 'validator').settings;
-    var oldErrorFunction = settngs.errorPlacement;
-    var oldSucessFunction = settngs.success;
-    settngs.errorPlacement = function (error, element) {
-        $(element).parent().addClass("input-validation-error");
-        oldErrorFunction(error, element);
-    };
-    settngs.success = function (label, element) {
-        $(element).parent().removeClass("input-validation-error");
-        oldSucessFunction(label, element);
-    };
+    $("form").each(function () {
+        var validator = $.data(this, 'validator');
+        var settings = validator.settings;
+        var oldErrorFunction = settings.errorPlacement;
+        var oldSuccessFunction = settings.success;
+        settings.errorPlacement = function (error, element) {
+            $(element).parent().addClass("input-validation-error");
+            oldErrorFunction(error, element);
+        };
+        settings.success = function (label, element) {
+            $(element).parent().removeClass("input-validation-error");
+            oldSuccessFunction(label, element);
+        };
+    });
 });
