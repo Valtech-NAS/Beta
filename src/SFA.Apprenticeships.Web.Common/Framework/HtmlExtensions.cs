971c2e713d385e1d20fb5758edab6827252341a0
kristerbone <krister.bone@valtech.co.uk>
2014-07-03 18:40:46 +0100

    Updated display control to support customising both container and control attributes. Added anchor tag and characters left span (needs to be dynamically calculated)

--- src/SFA.Apprenticeships.Web.Common/Framework/HtmlExtensions.cs
+++ src/SFA.Apprenticeships.Web.Common/Framework/HtmlExtensions.cs
@@ -1,8 +1,8 @@
-﻿
-namespace SFA.Apprenticeships.Web.Common.Framework
+﻿namespace SFA.Apprenticeships.Web.Common.Framework
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
     using System.Web.Mvc;
     using System.Web.Mvc.Html;
@@ -20,8 +20,8 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             Expression<Func<TModel, TProperty>> expression,
             string labelText = null,
             string hintText = null,
-            object htmlAttributes = null,
-            string textHtmlAttributes = null)
+            object containerHtmlAttributes = null,
+            object controlHtmlAttributes = null)
         {
             Condition.Requires(helper, "helper").IsNotNull();
             Condition.Requires(expression, "expression").IsNotNull();
@@ -29,6 +29,13 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             ModelState modelState;
             var validationError = false;
             var name = ExpressionHelper.GetExpressionText(expression);
+            RouteValueDictionary controlAttributes = containerHtmlAttributes != null ? new RouteValueDictionary(controlHtmlAttributes) : new RouteValueDictionary();
+           
+            if (controlAttributes.ContainsKey("class")){
+                controlAttributes["class"] += " form-control";
+            }else{
+                controlAttributes.Add("class", "form-control");
+            }
 
             if (!string.IsNullOrEmpty(name) && helper.ViewData.ModelState.TryGetValue(name, out modelState))
             {
@@ -38,9 +45,11 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             return FormText(
                 helper.LabelFor(expression, labelText, new { @class = "form-label" }).ToString(),
                 helper.HintFor(expression, hintText, new { @class = "form-hint" }).ToString(),
-                helper.TextBoxFor(expression, new { @class = textHtmlAttributes == null ? "form-control" : "form-control " + textHtmlAttributes }).ToString(),
+                helper.TextBoxFor(expression, controlAttributes).ToString(),
+                AnchorFor(helper, expression).ToString(),
+                string.Empty,
                 validationError,
-                htmlAttributes);
+                containerHtmlAttributes);
         }
 
         /// <summary>
@@ -52,7 +61,8 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             Expression<Func<TModel, TProperty>> expression,
             string labelText = null,
             string hintText = null,
-            object htmlAttributes = null)
+            object containerHtmlAttributes = null,
+            object controlHtmlAttributes = null)
         {
             Condition.Requires(helper, "helper").IsNotNull();
             Condition.Requires(expression, "expression").IsNotNull();
@@ -60,6 +70,14 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             ModelState modelState;
             var validationError = false;
             var name = ExpressionHelper.GetExpressionText(expression);
+
+            RouteValueDictionary controlAttributes = containerHtmlAttributes != null ? new RouteValueDictionary(controlHtmlAttributes) : new RouteValueDictionary();
+
+            if (controlAttributes.ContainsKey("class")){
+                controlAttributes["class"] += " form-control";
+            }else{
+                controlAttributes.Add("class", "form-control");
+            }
 
             if (!string.IsNullOrEmpty(name) && helper.ViewData.ModelState.TryGetValue(name, out modelState))
             {
@@ -69,28 +87,39 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             return FormText(
                 helper.LabelFor(expression, labelText, new { @class = "form-label" }).ToString(),
                 helper.HintFor(expression, hintText, new { @class = "form-hint" }).ToString(),
-                helper.TextAreaFor(expression, new { @class = "form-control" }).ToString(),
+                helper.TextAreaFor(expression, controlAttributes).ToString(),
+                AnchorFor(helper, expression).ToString(),
+                CharactersLeftFor(helper, expression).ToString(),
                 validationError,
-                htmlAttributes);
+                containerHtmlAttributes);
         }
 
-        private static MvcHtmlString FormText(string labelContent, string hintContent, string fieldContent, bool validationError = false, object htmlAttributes = null)
+        private static MvcHtmlString FormText(string labelContent, 
+                                            string hintContent, 
+                                            string fieldContent, 
+                                            string anchorTag,
+                                            string maxLengthSpan,
+                                            bool validationError = false, 
+                                            object containerHtmlAttributes = null)
         {
             var container = new TagBuilder("div");
-            container.AddCssClass("form-group");
+            
             if (validationError)
             {
                 container.AddCssClass(HtmlHelper.ValidationInputCssClassName);
             }
 
-            if (htmlAttributes != null)
+            if (containerHtmlAttributes != null)
             {
-                container.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+                container.MergeAttributes(new RouteValueDictionary(containerHtmlAttributes));
             }
+            container.AddCssClass("form-group");
 
+            container.InnerHtml += anchorTag;
             container.InnerHtml += labelContent;
             container.InnerHtml += hintContent;
             container.InnerHtml += fieldContent;
+            container.InnerHtml += maxLengthSpan;
 
             return MvcHtmlString.Create(container.ToString());
         }
@@ -119,6 +148,39 @@ namespace SFA.Apprenticeships.Web.Common.Framework
             }
 
             tag.SetInnerText(labelText);
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        }
+
+        private static MvcHtmlString AnchorFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+        {
+            Condition.Requires(helper, "helper").IsNotNull();
+            Condition.Requires(expression, "expression").IsNotNull();
+
+            var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+            var elementId = helper.ViewData.TemplateInfo.GetFullHtmlFieldId(metadata.PropertyName);
+            
+            if (string.IsNullOrWhiteSpace(elementId))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            var tag = new TagBuilder("a");
+            tag.Attributes.Add("href", "#" + elementId.ToLower());
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        }
+
+        private static MvcHtmlString CharactersLeftFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+        {
+            Condition.Requires(helper, "helper").IsNotNull();
+            Condition.Requires(expression, "expression").IsNotNull();
+
+            var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+
+            var tag = new TagBuilder("span");
+            tag.Attributes.Add("class", "form-hint maxchar-count");
+            
+            //TODO: This needs to be calculated dyamically either by refelction from from the generated HTML of the control.
+            tag.SetInnerText("4000");
             return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
         }
     }
