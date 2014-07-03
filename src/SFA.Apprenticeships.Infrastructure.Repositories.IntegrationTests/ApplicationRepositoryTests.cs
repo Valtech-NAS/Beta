82026cc1d2a6136c19799e880a010666aed614e6
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-03 17:22:14 +0100

    added repo test for applications

---
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.IntegrationTests/ApplicationRepositoryTests.cs
@@ -0,0 +1,46 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Repositories.IntegrationTests
+{
+    using System;
+    using Applications.IoC;
+    using Common.IoC;
+    using Domain.Entities.Applications;
+    using Domain.Interfaces.Repositories;
+    using NUnit.Framework;
+    using StructureMap;
+
+    [TestFixture]
+    public class ApplicationRepositoryTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.AddRegistry<ApplicationRepositoryRegistry>();
+                x.AddRegistry<CommonRegistry>();
+            });
+        }
+
+        [Test]
+        public void ShouldCreateApplication()
+        {
+            // arrange
+            var writer = ObjectFactory.GetInstance<IApplicationWriteRepository>();
+            var application = CreateTestApplication();
+
+            // act, assert
+            var savedApplication = writer.Save(application);
+        }
+
+        #region Helpers
+        private static ApplicationDetail CreateTestApplication()
+        {
+            return new ApplicationDetail
+            {
+                Id = 111
+            };
+        }
+
+        #endregion
+    }
+}
