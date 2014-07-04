7c8999bb316413245cc6ca31a7319964c3fa72ed
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-04 15:57:59 +0100

    added proxy for new legacy integration w/s and code tidy

---
+++ src/SFA.Apprenticeships.Infrastructure.LegacyWebServices/CreateApplication/LegacyApplicationRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.LegacyWebServices.CreateApplication
+{
+    using System;
+
+    public class LegacyApplicationRepository
+    {
+        //todo: LegacyApplicationRepository w/s integration
+    }
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.LegacyWebServices/CreateCandidate/LegacyCandidateRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.LegacyWebServices.CreateCandidate
+{
+    using System;
+
+    public class LegacyCandidateRepository
+    {
+        //todo: LegacyCandidateRepository w/s integration
+    }
+}
