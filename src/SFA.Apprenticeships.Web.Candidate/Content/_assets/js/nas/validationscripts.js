dcfeb531bb8aa1fba047f9450f012cecbe3bfab6
kristerbone <krister.bone@valtech.co.uk>
2014-07-07 17:04:48 +0100

    Checking validation for Chris to view, no finished

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/validationscripts.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/validationscripts.js
@@ -1,34 +1,5 @@
 ﻿$(document).ready(function() {
 
-    //$.validator.prototype.resetSummary = function () {
-    //    var form = $(this.currentForm);
-    //    form.find("[data-valmsg-summary=true]")
-    //        .removeClass("validation-summary-errors")
-    //        .addClass("validation-summary-valid")
-    //        .find("ul")
-    //        .empty();
-    //    return this;
-    //};
-
-    //$.validator.setDefaults({
-    //    showErrors: function (errorMap, errorList) {
-    //        this.defaultShowErrors();
-    //        this.checkForm();
-    //        if (this.errorList.length) {
-    //            $(this.currentForm).triggerHandler("invalid-form", [this]);
-    //        } else {
-    //            this.resetSummary();
-    //        }
-    //    }
-    //});
-
-    //$("form").each(function() {
-    //    var validator = $.data(this, 'validator');
-    //    validator.setDefaults({
-    //        debug: true
-    //    });
-    //});
-
     $("form").each(function () {
         var validator = $.data(this, 'validator');
         var settings = validator.settings;
@@ -43,6 +14,22 @@
             oldSuccessFunction(label, element);
 
         };
+        settings.showErrors = function(errorMap, errorList) {
+            //this.defaultShowErrors();
+            //$(".validation-summary-errors > div.panel-body > ul").empty();
+            $.each(errorList, function(index, error) {
+                //$(".validation-summary-errors > div.panel-body > ul").append("<li><a href='#" + error.element.id.toLowerCase() + "'>" + error.message + "</a></li>");
+                error.message = "<b>" + error.message + "</b>";
+            });
+            this.defaultShowErrors();
+        };
+        settings.errorPlacement = function (error, element) {
+            if (showErrorMessage) {
+                var li = document.createElement("li");
+                li.appendChild(document.createTextNode(error.html()));
+                $ul.append(li);
+            }
+        },
     });
 
     //$.validator.addMethod("fraction", function (value, element, param) {
---
+++ src/SFA.Apprenticeships.Web.Candidate/Validators/EducationViewModelValidator.cs
@@ -0,0 +1,64 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.Validators
+{
+    using System;
+    using Constants.ViewModels;
+    using FluentValidation;
+    using ViewModels.Candidate;
+
+    public class EducationViewModelClientValidator : AbstractValidator<EducationViewModel>
+    {
+        public EducationViewModelClientValidator()
+        {
+            this.AddCommonRules();
+        }
+    }
+
+    public class EducationViewModelServerValidator : AbstractValidator<EducationViewModel>
+    {
+        public EducationViewModelServerValidator()
+        {
+            this.AddCommonRules();
+            this.AddServerRules();
+        }
+    }
+
+    internal static class EducationValidaitonRules
+    {
+        internal static void AddCommonRules(this AbstractValidator<EducationViewModel> validator)
+        {
+            validator.RuleFor(x => x.NameOfMostRecentSchoolCollege)
+                .Length(0, 120)
+                .WithMessage(EducationMessages.NameOfMostRecentSchoolCollegeMessages.TooLongErrorText)
+                .NotEmpty()
+                .WithMessage(EducationMessages.NameOfMostRecentSchoolCollegeMessages.RequiredErrorText)
+                .Matches(EducationMessages.NameOfMostRecentSchoolCollegeMessages.WhiteListRegularExpression)
+                .WithMessage(EducationMessages.NameOfMostRecentSchoolCollegeMessages.WhiteListErrorText);
+
+            validator.RuleFor(x => x.FromYear)
+                .NotEmpty()
+                .WithMessage(EducationMessages.FromYearMessages.RequiredErrorText)
+                .Matches(EducationMessages.FromYearMessages.WhiteListRegularExpression)
+                .WithMessage(EducationMessages.FromYearMessages.WhiteListErrorText);
+
+            validator.RuleFor(x => x.ToYear)
+                .NotEmpty()
+                .WithMessage(EducationMessages.ToYearMessages.RequiredErrorText)
+                .Matches(EducationMessages.FromYearMessages.WhiteListRegularExpression)
+                .WithMessage(EducationMessages.FromYearMessages.WhiteListErrorText);
+        }
+
+        internal static void AddServerRules(this AbstractValidator<EducationViewModel> validator)
+        {
+            validator.RuleFor(x => x.ToYear)
+                .Must(BeBeforeOrEqual)
+                .WithMessage(EducationMessages.ToYearMessages.BeforeOrEqualErrorText);
+        }
+
+        private static bool BeBeforeOrEqual(EducationViewModel instance, string toYear)
+        {
+            var to = int.Parse(toYear);
+            var from = int.Parse(instance.FromYear);
+            return from <= to && from <= DateTime.Now.Year;
+        }
+    }
+}
