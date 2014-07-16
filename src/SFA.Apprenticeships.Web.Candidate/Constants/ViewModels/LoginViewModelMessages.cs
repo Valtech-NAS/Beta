66976643eca5ecbc502bcea224080653a5d36810
alangorton <alan.gorton@valtech.co.uk>
2014-07-16 15:59:02 +0100

    Added login page, related controller, view model, validators etc

---
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/ViewModels/LoginViewModelMessages.cs
@@ -0,0 +1,22 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.Constants.ViewModels
+{
+    public class LoginViewModelMessages
+    {
+        public static class EmailAddressMessages
+        {
+            public const string LabelText = "Email address";
+            public const string RequiredErrorText = "'Email address' must be supplied";
+        }
+
+        public static class PasswordMessages
+        {
+            public const string LabelText = "Password";
+            public const string RequiredErrorText = "'Password' must be supplied";
+        }
+
+        public static class AuthenticationMessages
+        {
+            public const string AuthenticationFailedErrorText = "'Email address' or 'password' is invalid";
+        }
+    }
+}
