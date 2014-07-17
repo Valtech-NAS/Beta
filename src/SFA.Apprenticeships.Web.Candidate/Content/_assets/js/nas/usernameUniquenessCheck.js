c245ff70a91d8cdb439f11386078794bc715301b
Christopher Monney <chris.monney@gmail.com>
2014-07-17 12:18:34 +0100

    Added Readme text and javascript to check username uniqueness

---
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/nas/usernameUniquenessCheck.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+
+    $('input[id=EmailAddress').focusout(function() {
+        var request = new UsernameModel();
+
+        alert(JSON.stringify(request));
+    });
+
+});
+
+function UsernameModel() {
+    var self = this;
+    self.email = $('input[id=EmailAddress').val();
+}
