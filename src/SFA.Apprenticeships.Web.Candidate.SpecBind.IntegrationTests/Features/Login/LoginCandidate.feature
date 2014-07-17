881075b52434ea3b703ddb1ee9f376c978c4d08f
alangorton <alan.gorton@valtech.co.uk>
2014-07-17 10:47:30 +0100

    Added login feature tests (SpecBind)

---
+++ src/SFA.Apprenticeships.Web.Candidate.SpecBind.IntegrationTests/Features/Login/LoginCandidate.feature
@@ -0,0 +1,26 @@
+﻿@US415
+Feature: Login Candidate
+	As a candidate,
+	I want to sign in so that I can access my profile,
+	apply for apprenticeships etc.
+
+Scenario: As a candidate all required fields are present
+	Given I navigated to the LoginCandidatePage page
+	When I am on the LoginCandidatePage page
+	Then I wait to see EmailAddress
+	And I wait to see Password
+
+Scenario: As a candidate I must provide an email address and password
+	Given I navigated to the LoginCandidatePage page
+	When I am on the LoginCandidatePage page
+	When I choose SignInButton
+
+Scenario: As a candidate I can login with a registered and activated email address and password
+	Given I navigated to the LoginCandidatePage page
+	When I am on the LoginCandidatePage page
+	And I enter data
+		| Field			| Value			|
+		| EmailAddress	| valtechnas@gmail.com	|
+		| Password		| ?Password01!		|
+	And I choose SignInButton
+	Then I am on the VacancySearchPage page
---
+++ src/SFA.Apprenticeships.Web.Candidate.SpecBind.IntegrationTests/Pages/Login/LoginCandidatePage.cs
@@ -0,0 +1,42 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.SpecBind.IntegrationTests.Pages.Login
+{
+    using global::SpecBind.Pages;
+    using global::SpecBind.Selenium;
+    using OpenQA.Selenium;
+
+    [PageNavigation("/login")]
+    [PageAlias("LoginCandidatePage")]
+    public class LoginCandidatePage
+    {
+        private readonly ISearchContext _context;
+
+        public LoginCandidatePage(ISearchContext context)
+        {
+            _context = context;
+        }
+
+        [ElementLocator(Id = "EmailAddress")]
+        public IWebElement EmailAddress { get; set; }
+
+        [ElementLocator(Id = "Password")]
+        public IWebElement Password { get; set; }
+
+        // TODO: AG: triggerSignIn button id.
+        [ElementLocator(Id = "triggerSignIn")]
+        public IWebElement SignInButton { get; set; }
+
+        [ElementLocator(Class = "validation-summary-errors")]
+        public IElementList<IWebElement, ValidationSummaryItem> ValidationSummary { get; set; }
+
+        public class ValidationSummaryItem : WebElement
+        {
+            public ValidationSummaryItem(ISearchContext parent)
+                : base(parent)
+            {
+            }
+
+            [ElementLocator(CssSelector = "a")]
+            public IWebElement Link { get; set; }
+        }
+    }
+}
