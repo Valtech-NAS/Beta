84e85cc4f0de3cfb475f448953ffc16b82f57246
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-21 15:15:17 +0100

    comment update

--- src/SFA.Apprenticeships.Domain.Entities/Users/UserStatuses.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Users/UserStatuses.cs
@@ -7,7 +7,7 @@
         Unknown = 0,
         PendingActivation = 10,     // once registered and awaiting activation
         Active = 20,                // once activated
-        Inactive = 30,              // when superseded by a new account
-        Blocked = 90                // if locked out for security reasons
+        Inactive = 30,              // when superseded by a new account (user changed their email address)
+        Locked = 90                 // if locked out for security reasons
     }
 }
