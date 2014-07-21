ee5281d499e4e619db699d8fdd466ac4ca061369
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-21 15:43:45 +0100

    comment update

--- src/SFA.Apprenticeships.Application.Candidate/Strategies/LegacySubmitApplicationStrategy.cs
+++ src/SFA.Apprenticeships.Application.Candidate/Strategies/LegacySubmitApplicationStrategy.cs
@@ -9,9 +9,9 @@
 
         public void SubmitApplication(ApplicationDetail application)
         {
-            // TODO: NOTIMPL: create/update application status
             // TODO: NOTIMPL: queue application for submission to legacy
-            // TODO: NOTIMPL: send email acknowledgement to candidate via ICommunicationService.SendMessageToCandidate()
+            // TODO: NOTIMPL: if successful then update application status
+            // TODO: NOTIMPL: then send email acknowledgement to candidate via ICommunicationService.SendMessageToCandidate()
 
             throw new NotImplementedException();
         }
