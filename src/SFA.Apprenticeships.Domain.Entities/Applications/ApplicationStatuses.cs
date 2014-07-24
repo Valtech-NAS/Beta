775642eb3c75720827ab35f64fcf5d7e8851c752
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-24 16:20:16 +0100

    updated app status enum

--- src/SFA.Apprenticeships.Domain.Entities/Applications/ApplicationStatuses.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Applications/ApplicationStatuses.cs
@@ -5,9 +5,10 @@
         Unknown = 0,
         Draft = 10,         // created but not yet submitted
         Expired = 15,       // draft that passed the vacancy closing date and cannot be submitted
-        Submitted = 20,     // submitted and awaiting vacancy manager review
-        InProgress = 25,    // reviewed by vacancy manager and being processed along with other applications
-        Successful = 90,    // processed by vacancy manager and marked as successful
-        Unsuccessful = 95   // processed by vacancy manager and marked as unsuccessful
+        Submitting = 20,    // user has submitted to be processed
+        Submitted = 30,     // submitted and awaiting vacancy manager review
+        InProgress = 40,    // reviewed by vacancy manager and being processed along with other applications
+        Successful = 80,    // processed by vacancy manager and marked as successful
+        Unsuccessful = 90   // processed by vacancy manager and marked as unsuccessful
     }
 }
