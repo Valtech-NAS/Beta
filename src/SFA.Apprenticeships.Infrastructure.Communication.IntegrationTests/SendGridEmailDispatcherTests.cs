57d0d1048e876ac22ad789be4360049f320f2820
Christopher Monney <chris.monney@gmail.com>
2014-07-26 23:08:31 +0100

    Fixed SendGrid Email Dispatcher Test

--- src/SFA.Apprenticeships.Infrastructure.Communication.IntegrationTests/SendGridEmailDispatcherTests.cs
+++ src/SFA.Apprenticeships.Infrastructure.Communication.IntegrationTests/SendGridEmailDispatcherTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Configuration;
     using Application.Interfaces.Messaging;
     using Common.IoC;
     using Domain.Interfaces.Configuration;
