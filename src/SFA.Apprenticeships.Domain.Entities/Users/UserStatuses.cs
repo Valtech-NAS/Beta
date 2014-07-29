998662ab5b92a6d997915861f4634ea4a86d48e7
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-07-29 14:08:55 +0100

    added dormant user account status (currently unused)

--- src/SFA.Apprenticeships.Domain.Entities/Users/UserStatuses.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Users/UserStatuses.cs
@@ -7,7 +7,8 @@
         Unknown = 0,
         PendingActivation = 10,     // once registered and awaiting activation
         Active = 20,                // once activated
-        Inactive = 30,              // when superseded by a new account (user changed their email address)
-        Locked = 90                 // if locked out for security reasons
+        Inactive = 30,              // when superseded by a new account (user changed their username)
+        Locked = 90,                // if locked out for security reasons
+        Dormant = 100               // if the account is no longer in use (reqts are TBC)
     }
 }
