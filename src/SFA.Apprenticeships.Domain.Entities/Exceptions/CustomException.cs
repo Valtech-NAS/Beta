1a9f187ee10e2b4a84c2ce33ab326dbd86b78a78
Christopher Monney <chris.monney@gmail.com>
2014-07-29 09:18:15 +0100

    Added custom exception class and error codes

---
+++ src/SFA.Apprenticeships.Domain.Entities/Exceptions/CustomException.cs
@@ -0,0 +1,38 @@
+﻿namespace SFA.Apprenticeships.Domain.Entities.Exceptions
+{
+    using System;
+
+    public class CustomException : Exception
+    {
+        public CustomException(ErrorCodes code)
+        {
+            Code = code;
+        }
+
+        public CustomException(string message, ErrorCodes code)
+            : base(message)
+        {
+            Code = code;
+        }
+
+        public CustomException(string format, ErrorCodes code, params object[] args)
+            : base(string.Format(format, args))
+        {
+            Code = code;
+        }
+
+        public CustomException(string message, Exception innerException, ErrorCodes code)
+            : base(message, innerException)
+        {
+            Code = code;
+        }
+
+        public CustomException(string format, Exception innerException, ErrorCodes code, params object[] args)
+            : base(string.Format(format, args), innerException)
+        {
+            Code = code;
+        }
+
+        public ErrorCodes Code { get; private set; }
+    }
+}
---
+++ src/SFA.Apprenticeships.Domain.Entities/Exceptions/ErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.Apprenticeships.Domain.Entities.Exceptions
+{
+    public class ErrorCodes
+    {
+        public const string UnknownUserError = "User001";
+        public const string UnknownCandidateError = "Candidate001";
+    }
+}
