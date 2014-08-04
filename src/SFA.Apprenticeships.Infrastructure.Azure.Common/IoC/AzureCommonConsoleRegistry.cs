4dff742eeef818dc14acf58491bf404f1e659068
kristerbone <krister.bone@valtech.co.uk>
2014-08-04 11:14:27 +0100

    Re-added vacancy etl application as needed to allow us to put manually created scheduled events on the Azure control queue

---
+++ src/SFA.Apprenticeships.Infrastructure.Azure.Common/IoC/AzureCommonConsoleRegistry.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Azure.Common.IoC
+{
+    using Configuration;
+    using StructureMap.Configuration.DSL;
+
+    public class AzureCommonConsoleRegistry : Registry
+    {
+        public AzureCommonConsoleRegistry()
+        {
+            For<IAzureCloudConfig>().Singleton().Use<AzureConsoleCloudConfig>();
+            For<IAzureCloudClient>().Use<AzureCloudClient>();
+        }
+    }
+}
