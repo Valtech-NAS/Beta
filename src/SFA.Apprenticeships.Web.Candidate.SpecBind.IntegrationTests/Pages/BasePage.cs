f417f6787f07a6036cd5a7f77731ef4727509b63
kristerbone <krister.bone@valtech.co.uk>
2014-08-04 13:34:05 +0100

    Removed todo on validation page and created basepage

---
+++ src/SFA.Apprenticeships.Web.Candidate.SpecBind.IntegrationTests/Pages/BasePage.cs
@@ -0,0 +1,26 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.SpecBind.IntegrationTests.Pages
+{
+    using OpenQA.Selenium;
+
+    public class BasePage
+    {
+        protected readonly ISearchContext Context;
+        protected readonly IWebDriver Driver;
+
+        public BasePage(ISearchContext context)
+        {
+            Context = context;
+        }
+
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public BasePage(IWebDriver driver, ISearchContext context)
+        {
+            Context = context;
+            Driver = driver;
+        }
+    }
+}
