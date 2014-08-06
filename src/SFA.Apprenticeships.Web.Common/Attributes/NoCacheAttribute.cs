8d9123519b554de64160925257fbd5fc67a91f1c
alangorton <alan.gorton@valtech.co.uk>
2014-08-06 13:15:29 +0100

    Added NoCacheAttribute based on OutputCacheAttribute - applied it to register/activation only

---
+++ src/SFA.Apprenticeships.Web.Common/Attributes/NoCacheAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.Apprenticeships.Web.Common.Attributes
+{
+    using System.Web.Mvc;
+
+    public class NoCacheAttribute : OutputCacheAttribute
+    {
+        public NoCacheAttribute()
+        {
+            NoStore = true;
+            Duration = 0;
+            VaryByParam = "None";
+        }
+    }
+}
