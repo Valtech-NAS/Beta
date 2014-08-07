e02cb8ab62a3c654ca637100e00b99fd24149716
kristerbone <krister.bone@valtech.co.uk>
2014-08-07 13:42:16 +0100

    Added rabbit logging override to log rabbit messages to the ELK stack

---
+++ src/SFA.Apprenticeships.Infrastructure.RabbitMq/ServiceOverrides/EasyNetQLogger.cs
@@ -0,0 +1,31 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.RabbitMq.ServiceOverrides
+{
+    using System;
+    using EasyNetQ;
+    using NLog;
+
+    public class EasyNetQLogger : IEasyNetQLogger
+    {
+        private static readonly Logger Logger = LogManager.GetLogger("RabbitMQ");
+        
+        public void DebugWrite(string format, params object[] args)
+        {
+            Logger.Debug(format, args);
+        }
+
+        public void InfoWrite(string format, params object[] args)
+        {
+            Logger.Info(format, args);
+        }
+
+        public void ErrorWrite(string format, params object[] args)
+        {
+            Logger.Error(format, args);
+        }
+
+        public void ErrorWrite(Exception exception)
+        {
+            Logger.Error(exception);
+        }
+    }
+}
