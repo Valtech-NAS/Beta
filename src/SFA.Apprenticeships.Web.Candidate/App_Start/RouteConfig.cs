821efc7f8286fb0ca11c105524c199eed6cbe208
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-08-11 17:12:56 +0100

    added named routed for dashboard

--- src/SFA.Apprenticeships.Web.Candidate/App_Start/RouteConfig.cs
+++ src/SFA.Apprenticeships.Web.Candidate/App_Start/RouteConfig.cs
@@ -13,6 +13,12 @@
             routes.IgnoreRoute("{folder}/{*pathInfo}", new { folder = "Scripts" });
 
             routes.MapRoute(
+                name: "MyApplications",
+                url: "myapplications",
+                defaults: new { controller = "Application", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
