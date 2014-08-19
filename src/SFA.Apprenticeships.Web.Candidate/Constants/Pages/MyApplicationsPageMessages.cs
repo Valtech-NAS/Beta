930f44d8299d691c3c28d5bb488dbf727adfcba0
Henry Charge <henry.charge@valtech.co.uk>
2014-08-19 08:17:10 +0000

    Content changes for success/warning messages

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/MyApplicationsPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/MyApplicationsPageMessages.cs
@@ -2,7 +2,7 @@
 {
     public class MyApplicationsPageMessages
     {
-        public const string DraftExpired = "TODO: The vacancy for this draft application is no longer available";
-        public const string ApplicationArchived = "TODO: The application has been archived";
+        public const string DraftExpired = "This apprenticeship is no longer available";
+        public const string ApplicationArchived = "Application has been removed from your drafts";
     }
 }
