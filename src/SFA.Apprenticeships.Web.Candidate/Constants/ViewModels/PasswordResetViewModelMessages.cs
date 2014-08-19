3f22b3cd691a732e9dea6e98e8b4a43c065d8a47
Henry Charge <henry.charge@valtech.co.uk>
2014-08-19 14:11:01 +0000

    Update incorrect password text

--- src/SFA.Apprenticeships.Web.Candidate/Constants/ViewModels/PasswordResetViewModelMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/ViewModels/PasswordResetViewModelMessages.cs
@@ -15,7 +15,7 @@
         public static class PasswordMessages
         {
             public const string LabelText = "New password";
-            public const string HintText = "Requires upper and lowercase letters, a number at least 8 characters";
+            public const string HintText = "Requires upper and lowercase letters, a number and at least 8 characters";
             public const string RequiredErrorText = "'New Password' must be supplied";
             public const string LengthErrorText = "'New Password' must be at least 8 characters long";
             public const string WhiteListRegularExpression = Whitelists.PasswordWhitelist.RegularExpression;
