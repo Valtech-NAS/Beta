62d0c1181c6e0480308e0b9a8fb8190c8366e2db
Henry Charge <henry.charge@valtech.co.uk>
2014-08-20 15:11:00 +0000

    Error messages for about you

--- src/SFA.Apprenticeships.Web.Candidate/Constants/ViewModels/AboutYouViewModelMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/ViewModels/AboutYouViewModelMessages.cs
@@ -8,8 +8,8 @@
         {
             public const string LabelText = "Give examples of your strengths that are relevant to the apprenticeship";
             public const string HintText = "Don't just list your strengths. Whatever examples you give, you’ll need to provide details of when you’ve shown these.";
-            public const string RequiredErrorText = "'What are your strengths' must be supplied";
-            public const string TooLongErrorText = "'What are your strengths' must not exceed 4000 characters";
+            public const string RequiredErrorText = "Please provide examples of your strengths";
+            public const string TooLongErrorText = "Examples of your strengths mustn’t exceed 4000 characters";
             public const string WhiteListRegularExpression = Whitelists.FreetextWhitelist.RegularExpression;
             public const string WhiteListErrorText = "'What are your strengths' " + Whitelists.FreetextWhitelist.ErrorText;
         }
@@ -18,8 +18,8 @@
         {
             public const string LabelText = "What skills would you like to improve during this apprenticeship?";
             public const string HintText = "Try and think of what your duties will be and the industry the employer works in.";
-            public const string RequiredErrorText = "'What do you feel you could improve' must be supplied";
-            public const string TooLongErrorText = "'What do you feel you could improve' must not exceed 4000 characters";
+            public const string RequiredErrorText = "Please provide examples of where you could improve";
+            public const string TooLongErrorText = "Examples of where you could improve mustn’t exceed 4000 characters";
             public const string WhiteListRegularExpression = Whitelists.FreetextWhitelist.RegularExpression;
             public const string WhiteListErrorText = "'What do you feel you could improve' " + Whitelists.FreetextWhitelist.ErrorText;
         }
@@ -28,8 +28,8 @@
         {
             public const string LabelText = "What are your hobbies and interests?";
             public const string HintText = "Think of the skills you’ve gained from your hobbies and interests that might be relevant. Include any personal achievements.";
-            public const string RequiredErrorText = "'What are your hobbies/interests' must be supplied";
-            public const string TooLongErrorText = "'What are your hobbies/interests' must not exceed 4000 characters";
+            public const string RequiredErrorText = "Please provide examples of your hobbies/interests";
+            public const string TooLongErrorText = "Examples of your hobbies/interests mustn’t exceed 4000 characters";
             public const string WhiteListRegularExpression = Whitelists.FreetextWhitelist.RegularExpression;
             public const string WhiteListErrorText = "'What are your hobbies/interests' " + Whitelists.FreetextWhitelist.ErrorText;
         }
@@ -37,9 +37,9 @@
         public static class AnythingWeCanDoToSupportYourInterviewMessages
         {
             public const string LabelText = "Is there anything we can do to support your interview?";
-            public const string HintText = "Give examples below";
-            public const string RequiredErrorText = "'Is there anything we can do to support your interview' must be supplied";
-            public const string TooLongErrorText = "'Is there anything we can do to support your interview' must not exceed 4000 characters";
+            public const string HintText = "Provide examples below";
+            public const string RequiredErrorText = "Please provide details of interview support needed";
+            public const string TooLongErrorText = "Interview support details mustn’t exceed 4000 characters";
             public const string WhiteListRegularExpression = Whitelists.FreetextWhitelist.RegularExpression;
             public const string WhiteListErrorText = "'Is there anything we can do to support your interview' " + Whitelists.FreetextWhitelist.ErrorText;
         }
