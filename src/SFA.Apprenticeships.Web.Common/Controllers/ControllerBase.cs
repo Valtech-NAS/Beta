9dad6267a1ab54dd718d302c74dfb9ee0380308a
kristerbone <krister.bone@valtech.co.uk>
2014-08-22 12:13:14 +0100

    US489: Session/Authetication timeout added and ASP.NET session disabled.

--- src/SFA.Apprenticeships.Web.Common/Controllers/ControllerBase.cs
+++ src/SFA.Apprenticeships.Web.Common/Controllers/ControllerBase.cs
@@ -9,15 +9,5 @@
     public abstract class ControllerBase<TContextType> : Controller where TContextType : UserContext
     {
         public TContextType UserContext { get; protected set; } //todo: may move...
-
-        private new ISessionStateProvider Session
-        {
-            get { throw new InvalidOperationException("Use of session is not supported"); }
-        }
-
-        protected void ClearSession()
-        {
-            base.Session.Clear();
-        }
     }
 }
