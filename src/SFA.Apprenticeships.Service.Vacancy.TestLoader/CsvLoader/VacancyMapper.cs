060834664aefec580b41e1421df47e902a5de8d4
kristerbone <krister.bone@valtech.co.uk>
2014-08-27 11:22:02 +0100

    US449: Loads test vacancies from csv to test elasticsearch index

---
+++ src/SFA.Apprenticeships.Service.Vacancy.TestLoader/CsvLoader/VacancyMapper.cs
@@ -0,0 +1,17 @@
+﻿namespace SFA.Apprenticeships.Service.Vacancy.TestLoader.CsvLoader
+{
+    using Application.VacancyEtl.Entities;
+    using CsvHelper.Configuration;
+
+    public class VacancyMapper : CsvClassMap<VacancySummaryUpdate>
+    {
+        public VacancyMapper()
+        {
+            Map(m => m.Id).Name("Vacancy Ref");
+            Map(m => m.Title).Name("Title");
+            Map(m => m.Description).Name("Description");
+            Map(m => m.EmployerName).Name("Employer");
+            Map(m => m.ClosingDate).Name("Closing date");
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Service.Vacancy.TestLoader/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace SFA.Apprenticeships.Service.Vacancy.TestLoader
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Application.VacancyEtl.Entities;
+    using CsvHelper;
+    using CsvLoader;
+    using Infrastructure.Common.IoC;
+    using Infrastructure.Elastic.Common.IoC;
+    using Infrastructure.VacancyIndexer;
+    using Infrastructure.VacancyIndexer.IoC;
+    using NLog;
+    using StructureMap;
+
+    public class Program
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            if (!CheckArgs(args))
+            {
+                return;
+            }
+
+            ObjectFactory.Configure(c =>
+            {
+                c.AddRegistry<CommonRegistry>();
+                c.AddRegistry<ElasticsearchCommonRegistry>();
+                c.AddRegistry<VacancyIndexerRegistry>();
+            });
+
+            using (TextReader reader = File.OpenText(args[0]))
+            {
+                var csv = new CsvReader(reader);
+                csv.Configuration.RegisterClassMap<VacancyMapper>();
+                
+                _logger.Debug("Reading...");
+                var allCsvRows = csv.GetRecords<VacancySummaryUpdate>().ToList();
+
+                var indexer = ObjectFactory.GetInstance<IVacancyIndexerService>();
+
+                var indexDate = DateTime.Today;
+
+                indexer.CreateScheduledIndex(indexDate);
+
+                foreach (VacancySummaryUpdate vacancySummaryUpdate in allCsvRows)
+                {
+                    vacancySummaryUpdate.ScheduledRefreshDateTime = indexDate;
+                    indexer.Index(vacancySummaryUpdate);    
+                }
+                
+                indexer.SwapIndex(indexDate);
+            }
+        }
+
+        private static bool CheckArgs(string[] args)
+        {
+            if (args == null || args.Length != 1)
+            {
+                Console.WriteLine("The usage requires the following paramters:");
+                Console.WriteLine("SFA.Apprenticeships.Service.Vacancy.TestLoader.exe <PathToCsvData>");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
