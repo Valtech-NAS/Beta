027ff0c959ddcabc3a0725cbf9f1a2bd15123e73
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-09-05 09:28:57 +0100

    Added a module to remove eTags to improve performance.

---
+++ src/SFA.Apprenticeships.Web.Common/Modules/RemoveETagModule.cs
@@ -0,0 +1,22 @@
+﻿namespace SFA.Apprenticeships.Web.Common.Modules
+{
+    using System;
+    using System.Web;
+
+    public class RemoveETagModule : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        private void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("ETag");
+        }
+    }
+}
