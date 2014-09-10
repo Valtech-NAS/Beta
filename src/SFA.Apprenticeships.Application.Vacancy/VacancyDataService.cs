63a71202e5113fbb84f5dafb82fc610d0ecab633
alangorton <alan.gorton@valtech.co.uk>
2014-09-10 14:41:17 +0100

    Build error: fixed reference to message constant

--- src/SFA.Apprenticeships.Application.Vacancy/VacancyDataService.cs
+++ src/SFA.Apprenticeships.Application.Vacancy/VacancyDataService.cs
@@ -22,8 +22,9 @@
             catch (Exception e)
             {
                 var message = string.Format("Get vacancy failed for vacancy {0}.", vacancyId);
+
                 throw new Domain.Entities.Exceptions.CustomException(
-                    message, e, ErrorCodes.GetVacancyDetailFailed);
+                    message, e, ErrorCodes.GetVacancyDetailsFailed);
             }
         }
     }
