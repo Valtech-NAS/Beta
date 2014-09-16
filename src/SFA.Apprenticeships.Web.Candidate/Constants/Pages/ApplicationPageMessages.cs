179f591453a89e6f7b4f7b110108e7465faf4c98
charge-valtech <henry.charge@valtech.co.uk>
2014-09-16 10:56:01 +0100

    Message updates from Dave

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/ApplicationPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/ApplicationPageMessages.cs
@@ -2,12 +2,12 @@
 {
     public class ApplicationPageMessages
     {
-        public const string ArchiveFailed = "TODO: There's been a problem archiving your application. Please try again.";
-        public const string DeleteFailed = "TODO: There's been a problem deleting your application. Please try again.";
-        public const string SubmitApplicationFailed = "TODO: There's been a problem submitting your application. Please try again.";
-        public const string PreviewFailed = "TODO: There's been a problem processing your request. Please try again.";
-        public const string SaveFailed = "TODO: There's been a problem saving the application. Please try again.";
+        public const string ArchiveFailed = "There’s been a problem saving your application. Please try again.";
+        public const string DeleteFailed = "There’s been a problem deleting your application. Please try again.";
+        public const string SubmitApplicationFailed = "There's been a problem submitting your application. Please try again.";
+        public const string PreviewFailed = "There's been a problem processing your request. Please try again.";
+        public const string SaveFailed = "There's been a problem saving your application. Please try again.";
         public const string LeavingPageMessage = "You’re about to leave the page. Any changes you’ve made will be lost.";
-        public const string VacancyNotFound = "TODO: Vacancy not found.";
+        public const string VacancyNotFound = "Apprenticeship not found";
     }
 }
--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/MyApplicationsPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/MyApplicationsPageMessages.cs
@@ -3,9 +3,9 @@
     public class MyApplicationsPageMessages
     {
         public const string DraftExpired = "This apprenticeship is no longer available";
-        public const string ApplicationArchived = "TODO: Application has been removed";
-        public const string ApplicationDeleted = "TODO: Application has been removed from your drafts";
+        public const string ApplicationArchived = "Your application has been removed";
+        public const string ApplicationDeleted = "Your application has been removed from your drafts";
         public const string CreateOrRetrieveApplicationFailed = "There's been a problem creating or retrieving your application details. Please try again.";
-        public const string ApplicationInIncorrectState = "The application is not in draf state.";
+        public const string ApplicationInIncorrectState = "The application is not in draft state.";
     }
 }
