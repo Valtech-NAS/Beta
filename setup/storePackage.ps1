30a9c356d5d5452d9adeff0e60161cb958d4a35c
kristerbone <krister.bone@valtech.co.uk>
2014-09-18 18:31:42 +0100

    More tweaks to store script

--- setup/storePackage.ps1
+++ setup/storePackage.ps1
@@ -1,4 +1,5 @@
 param(
+	[string]$subscriptionName,
 	[string]$storageName,
 	[string]$storageKey,
 	[string]$storageContainer,
@@ -15,15 +16,18 @@ Write-Output "Storing package at: $srcPath for version: $version"
 Write-Output "Running Azure Imports"
 Import-Module "C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\ServiceManagement\Azure\*.psd1"
 Import-AzurePublishSettingsFile "G:\Azure\Pre-Production-9-12-2014-credentials.publishsettings"
+Set-AzureSubscription -CurrentStorageAccount $storageName -SubscriptionName $subscriptionName
 
 Write-Host "Files will be uploaded to $storageAccountName\$storage$storageContainer"
 $context = New-AzureStorageContext -StorageAccountName $storageName -StorageAccountKey $storageKey
 
 Write-Host "Copying files to Azure Storage" 
+
+#DON'T NEED THIS FILE AS THIS IS IN THE SECURE REPO.
 #$fqName = "$srcPath\ServiceConfiguration.Cloud.cscfg"
 #Set-AzureStorageBlobContent -Blob "$storeageFolder\$buildnumber\ServiceConfiguration.Cloud.cscfg" -Container "$storage" -File "$fqName" -Context $context -Force
 
 $fqName = "$srcPath\$projectName.cspkg"
 Write-Host "Copying file: $fqName to $storageContainer\$projectName-$version.cspkg"
 Set-AzureStorageBlobContent -Blob "$storageContainer\$projectName-$version.cspkg" -Container $storageName -File $fqName -Context $context -Force
-Write-Host "Copyied file: $fqName to $storageContainer\$projectName-$version.cspkg"
+Write-Host "Copied file: $fqName to $storageContainer\$projectName-$version.cspkg"
