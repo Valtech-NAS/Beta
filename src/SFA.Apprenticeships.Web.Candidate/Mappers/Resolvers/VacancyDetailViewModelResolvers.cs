b9f856ddd4776da65583be59dd0f51bfe34bbb21
Christopher Monney <chris.monney@gmail.com>
2014-09-29 14:57:10 +0100

    Fixed bug introduced during mapping change

--- src/SFA.Apprenticeships.Web.Candidate/Mappers/Resolvers/VacancyDetailViewModelResolvers.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Mappers/Resolvers/VacancyDetailViewModelResolvers.cs
@@ -1,8 +1,6 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.Mappers.Resolvers
 {
     using System;
-    using System.Net;
-    using System.Web.Http;
     using AutoMapper;
     using Domain.Entities.Vacancies;
 
@@ -12,22 +10,12 @@
         {
             protected override string ResolveCore(VacancyDetail vacancyDetail)
             {
-                return vacancyDetail.IsEmployerAnonymous ?
-                    vacancyDetail.AnonymousEmployerName :
-                    vacancyDetail.EmployerName;
+                return vacancyDetail.IsEmployerAnonymous
+                    ? vacancyDetail.AnonymousEmployerName
+                    : vacancyDetail.EmployerName;
             }
         }
 
-        public class WageResolver : ValueResolver<VacancyDetail, string>
-        {
-            protected override string ResolveCore(VacancyDetail vacancyDetail)
-            {
-                return vacancyDetail.WageType == WageType.Text ?
-                    vacancyDetail.WageDescription :
-                    string.Format("£{0:N2}", vacancyDetail.Wage);
-            }
-        }      
- 
         public class VacancyUrlResolver : ValueResolver<VacancyDetail, string>
         {
             protected override string ResolveCore(VacancyDetail vacancyDetail)
@@ -37,7 +25,17 @@
 
             private static string GetUri(string s)
             {
-                return new UriBuilder(s).Uri.ToString();
+                return string.IsNullOrEmpty(s) ? string.Empty : new UriBuilder(s).Uri.ToString();
+            }
+        }
+
+        public class WageResolver : ValueResolver<VacancyDetail, string>
+        {
+            protected override string ResolveCore(VacancyDetail vacancyDetail)
+            {
+                return vacancyDetail.WageType == WageType.Text
+                    ? vacancyDetail.WageDescription
+                    : string.Format("£{0:N2}", vacancyDetail.Wage);
             }
         }
     }
