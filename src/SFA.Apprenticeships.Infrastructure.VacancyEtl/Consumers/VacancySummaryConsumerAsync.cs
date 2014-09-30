4296d7a73ed8cbd20788981715cdb23e6c273f7e
Christopher Monney <chris.monney@gmail.com>
2014-09-30 14:48:56 +0100

    Etl testing and failover
    Signed-off-by: Christopher Monney <chris.monney@gmail.com>

--- src/SFA.Apprenticeships.Infrastructure.VacancyEtl/Consumers/VacancySummaryConsumerAsync.cs
+++ src/SFA.Apprenticeships.Infrastructure.VacancyEtl/Consumers/VacancySummaryConsumerAsync.cs
@@ -1,5 +1,6 @@
 ﻿namespace SFA.Apprenticeships.Infrastructure.VacancyEtl.Consumers
 {
+    using System;
     using System.Threading.Tasks;
     using EasyNetQ.AutoSubscribe;
     using Application.VacancyEtl.Entities;
@@ -17,7 +18,12 @@
         [AutoSubscriberConsumer(SubscriptionId = "VacancySummaryConsumerAsync")]
         public Task Consume(VacancySummaryUpdate vacancySummaryToIndex)
         {
-            return Task.Run(() => _vacancyIndexer.Index(vacancySummaryToIndex));
+            return Task.Run(() =>
+            {
+                _vacancyIndexer.Index(vacancySummaryToIndex);
+
+                throw new Exception("Something terrible occurred while indexing");
+            });
         }
     }
 }
