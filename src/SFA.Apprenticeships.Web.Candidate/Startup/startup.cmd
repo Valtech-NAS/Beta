8658ad6f3ab76bad77579a5c95b775b327b38aa4
kristerbone <krister.bone@valtech.co.uk>
2014-10-08 10:25:14 +0100

    updated to use powershell as server cmd doesn't work on 2012

--- src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
+++ src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
@@ -1,5 +1,5 @@
 ﻿@echo off
 @echo Installing “IPv4 Address and Domain Restrictions” feature
-%windir%\System32\ServerManagerCmd.exe -install Web-IP-Security
+PowerShell Install-WindowsFeature –Name Web-IP-Security
 @echo Unlocking configuration for “IPv4 Address and Domain Restrictions” feature
 %windir%\system32\inetsrv\AppCmd.exe unlock config -section:system.webServer/security/ipSecurity
