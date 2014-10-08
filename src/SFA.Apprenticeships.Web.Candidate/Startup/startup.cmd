9d9d2227b839de21fbbf7a5418b5771f8a38fff8
kristerbone <krister.bone@valtech.co.uk>
2014-10-08 13:15:00 +0100

    new command for enabling ip security

--- src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
+++ src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
@@ -1,5 +1,5 @@
 ﻿@echo off
 @echo Installing “IPv4 Address and Domain Restrictions” feature
-PowerShell Install-WindowsFeature –Name Web-IP-Security
+%windir%\system32\dism.exe /online /quiet /enable-feature /featurename:IIS-IPSecurity 
 @echo Unlocking configuration for “IPv4 Address and Domain Restrictions” feature
 %windir%\system32\inetsrv\AppCmd.exe unlock config -section:system.webServer/security/ipSecurity
