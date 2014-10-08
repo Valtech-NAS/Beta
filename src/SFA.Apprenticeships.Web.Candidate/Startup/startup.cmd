f966d83ff6ad223f6337e1826c7a154fd221d687
kristerbone <krister.bone@valtech.co.uk>
2014-10-08 10:09:51 +0100

    Update to IP restrictions - needs enabled on server

--- src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
+++ src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
@@ -1,1 +1,5 @@
-﻿%windir%\system32\inetsrv\AppCmd.exe unlock config -section:system.webServer/security/ipSecurity
+﻿@echo off
+@echo Installing “IPv4 Address and Domain Restrictions” feature
+%windir%\System32\ServerManagerCmd.exe -install Web-IP-Security
+@echo Unlocking configuration for “IPv4 Address and Domain Restrictions” feature
+%windir%\system32\inetsrv\AppCmd.exe unlock config -section:system.webServer/security/ipSecurity
