d7d1b5a3eb06268d60823766b9706204d9be728c
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-10-28 15:53:44 +0000

    Added new WindowsService to emulate AsyncProcessor in LocalEnvironment

---
+++ src/SFA.Apprenticeships.CustomHosts.AsyncProcessorService/AsyncProecessorService.cs
@@ -0,0 +1,100 @@
+﻿using System.Net;
+using System.Threading;
+using EasyNetQ;
+using SFA.Apprenticeships.Infrastructure.Common.IoC;
+using SFA.Apprenticeships.Infrastructure.Communication.IoC;
+using SFA.Apprenticeships.Infrastructure.LegacyWebServices.IoC;
+using SFA.Apprenticeships.Infrastructure.RabbitMq.Interfaces;
+using SFA.Apprenticeships.Infrastructure.RabbitMq.IoC;
+using SFA.Apprenticeships.Infrastructure.Repositories.Applications.IoC;
+using SFA.Apprenticeships.Infrastructure.Repositories.Candidates.IoC;
+
+namespace SFA.Apprenticeships.CustomHosts.AsyncProcessorService
+{
+    using System;
+    using System.Reflection;
+    using System.ServiceProcess;
+    using NLog;
+    using Infrastructure.AsyncProcessor.Consumers;
+    using Infrastructure.AsyncProcessor.IoC;
+    using StructureMap;
+
+    public partial class AsyncProecessorService : ServiceBase
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public AsyncProecessorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.Debug("OnStart called.");
+
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            Initialise();
+        }
+
+        protected override void OnStop()
+        {
+            Logger.Debug("AsyncProcessor OnStop called.");
+
+            // Kill the bus which will kill any subscriptions
+            ObjectFactory.GetInstance<IBus>().Advanced.Dispose();
+
+            // Give it 5 seconds to finish processing any in flight subscriptions.
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+
+        private static void Initialise()
+        {
+            try
+            {
+                Logger.Debug("AsyncProcessor Initialising...");
+
+                InitializeIoC();
+                InitializeRabbitMQSubscribers();
+
+                Logger.Debug("AsyncProcessor Initialised.");
+            }
+            catch (Exception e)
+            {
+                Logger.Error("AsyncProcessor Initialisation error.", e);
+            }
+        }
+
+        private static void InitializeRabbitMQSubscribers()
+        {
+            const string asyncProcessorSubscriptionId = "AsyncProcessor";
+            var bootstrapper = ObjectFactory.GetInstance<IBootstrapSubcribers>();
+
+            Logger.Debug("RabbitMQ initialising");
+
+            bootstrapper.LoadSubscribers(Assembly.GetAssembly(typeof (EmailRequestConsumerAsync)),
+                asyncProcessorSubscriptionId);
+
+            Logger.Debug("RabbitMQ initialised");
+        }
+
+        private static void InitializeIoC()
+        {
+            Logger.Debug("IoC container initialising");
+
+            ObjectFactory.Initialize(x =>
+            {
+                x.AddRegistry<CommonRegistry>();
+                x.AddRegistry<RabbitMqRegistry>();
+                x.AddRegistry<CommunicationRegistry>();
+                x.AddRegistry<CandidateRepositoryRegistry>();
+                x.AddRegistry<ApplicationRepositoryRegistry>();
+                x.AddRegistry<LegacyWebServicesRegistry>();
+                x.AddRegistry<AsyncProcessorRegistry>();
+            });
+
+            Logger.Debug("IoC container initialised");
+        }
+    }
+}
