dba460c80566a810372ce40818178d46cc8d53e0
kristerbone <krister.bone@valtech.co.uk>
2014-11-03 18:38:15 +0000

    Counters need to be added of app will fail

--- src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
+++ src/SFA.Apprenticeships.Web.Candidate/Startup/startup.cmd
@@ -4,4 +4,4 @@ echo Installing “IPv4 Address and Domain Restrictions” feature
 echo Unlocking configuration for “IPv4 Address and Domain Restrictions” feature
 %windir%\system32\inetsrv\AppCmd.exe unlock config -section:system.webServer/security/ipSecurity
 echo Create Performance Counters if necessary
-REM PowerShell -Version 2.0 -ExecutionPolicy Unrestricted ".\startup\startup.ps1"
+PowerShell -Version 2.0 -ExecutionPolicy Unrestricted ".\startup\startup.ps1"
