cb815c585f3108b9f4d1481c1a0de2e2cdb48e3c
kristerbone <krister.bone@valtech.co.uk>
2014-11-05 08:41:12 +0000

    Fixing Webtrends bug, jQuery not available until page loads so this errored. Webtrends loads in the head so can call straight away.

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/ActivationPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/ActivationPageMessages.cs
@@ -6,7 +6,7 @@
     {
         public const string ActivationCodeSent = "An activation code has been sent to {0}";
         //TODO: Check mechanism for calling activation complete analytics (see below)
-        public const string AccountActivated = "You've successfully activated your account <script>$(function() { Webtrends.multiTrack({ element: this, argsa: ['DCS.dcsuri', '/register/activation/complete', 'WT.dl', '0', 'WT.ti', 'Activation Complete'] }); })</script>";
+        public const string AccountActivated = "You've successfully activated your account <script>Webtrends.multiTrack({ element: this, argsa: ['DCS.dcsuri', '/register/activation/complete', 'WT.dl', '0', 'WT.ti', 'Activation Complete'] });</script>";
         public const string ActivationCodeIncorrect = "Activation code entered is incorrect";
         public const string ActivationFailed = "There's been a problem activating your account. Please try again.";
         public const string ActivationCodeSendingFailure = "There's been a problem with your activation code. Please try again.";
