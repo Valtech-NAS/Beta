ce70a33441333925ca4882b254c2342d9de69293
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-11-05 13:32:24 +0000

    Removed bin directory from startup task

--- src/SFA.Apprenticeships.Infrastructure.Monitor/Startup/MonitorStartup.cmd
+++ src/SFA.Apprenticeships.Infrastructure.Monitor/Startup/MonitorStartup.cmd
@@ -1,7 +1,7 @@
 ﻿@echo off
 if "%EMULATED%"=="true" goto SKIP
 
-cd %ROLEROOT%\approot\bin\startup
+cd %ROLEROOT%\approot\startup
 
 reg add HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v ExecutionPolicy /d Unrestricted /f
 powershell .\MonitorStartup.ps1 >> StartUp.log 2>> StartUpError.log
--- src/SFA.Apprenticeships.Infrastructure.VacancyEtl/Startup/VacancyEtlStartup.cmd
+++ src/SFA.Apprenticeships.Infrastructure.VacancyEtl/Startup/VacancyEtlStartup.cmd
@@ -1,7 +1,7 @@
 ﻿@echo off
-REM if "%EMULATED%"=="true" goto SKIP
+if "%EMULATED%"=="true" goto SKIP
 
-cd %ROLEROOT%\approot\bin\startup
+cd %ROLEROOT%\approot\startup
 
 reg add HKLM\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell /v ExecutionPolicy /d Unrestricted /f
 powershell .\VacancyEtlStartup.ps1 >> StartUp.log 2>> StartUpError.log
