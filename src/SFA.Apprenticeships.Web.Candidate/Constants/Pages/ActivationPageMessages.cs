6f978083dd5fc6553e49745b2e3a9cde6d8f69e1
kristerbone <krister.bone@valtech.co.uk>
2014-11-05 16:56:55 +0000

    Webtrends account activation fixed

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/ActivationPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/ActivationPageMessages.cs
@@ -5,7 +5,7 @@
     public static class ActivationPageMessages
     {
         public const string ActivationCodeSent = "An activation code has been sent to {0}";
-        public const string AccountActivated = "You've successfully activated your account<span class=\"webtrendsTrack\" webTrendsMultiTrackArgs='{\"argsa\":[\"DCS.dcsuri\", \"/register/activation/complete\", \"WT.dl\", \"0\", \"WT.ti\", \"Activation Complete\"]}'></span>";
+        public const string AccountActivated = "You've successfully activated your account<span class=\"webtrendsTrack\" webTrendsMultiTrackArgs='{\"element\": null, \"argsa\":[\"DCS.dcsuri\", \"/register/activation/complete\", \"WT.dl\", \"0\", \"WT.ti\", \"Activation Complete\"]}'></span>";
         public const string ActivationCodeIncorrect = "Activation code entered is incorrect";
         public const string ActivationFailed = "There's been a problem activating your account. Please try again.";
         public const string ActivationCodeSendingFailure = "There's been a problem with your activation code. Please try again.";
