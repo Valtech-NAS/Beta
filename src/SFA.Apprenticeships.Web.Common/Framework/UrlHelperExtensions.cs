b0b2c3ba11fe7e28e2d495ba77faf112c5f97223
alangorton <alan.gorton@valtech.co.uk>
2014-11-10 16:45:43 +0000

    Azure CDN: get images to Azure CDN for non-local environments (SIT -> PROD).

---
+++ src/SFA.Apprenticeships.Web.Common/Framework/UrlHelperExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace SFA.Apprenticeships.Web.Common.Framework
+{
+    using System;
+    using System.Configuration;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    public static class UrlHelperExtensions
+    {
+        public static string CdnContent(this UrlHelper urlHelper, string contentName, string localContentPath)
+        {
+            const string separator = "/";
+
+            return IsLocalEnvironment ?
+                urlHelper.Content(string.Join(separator, localContentPath, contentName)) :
+                string.Join(separator, CdnUrl, EnvironmentName, contentName).ToLower();
+        }
+
+        #region Helpers
+
+        private static string EnvironmentName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["Environment"];
+            }
+        }
+
+        private static string CdnUrl
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["CdnUrl"];
+            }
+        }
+
+        private static bool IsLocalEnvironment
+        {
+            get
+            {
+                var localEnvironmentNames = new string[] { "debug", "local" };
+
+                return
+                    localEnvironmentNames.Any(each =>
+                        each.Equals(EnvironmentName, StringComparison.InvariantCultureIgnoreCase));
+            }
+        }
+
+        #endregion
+    }
+}
