27d7563f50a288b56d6f2c9266c3e81ed963d2e5
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-11-19 14:54:38 +0000

    Fixed integration tests of the new user directory provider.

--- src/SFA.Apprenticeships.Infrastructure.UserDirectory.IntegrationTests/UserDirectoryProviderTests.cs
+++ src/SFA.Apprenticeships.Infrastructure.UserDirectory.IntegrationTests/UserDirectoryProviderTests.cs
@@ -5,9 +5,11 @@
     using FluentAssertions;
     using IoC;
     using NUnit.Framework;
+    using SFA.Apprenticeships.Infrastructure.Common.IoC;
+    using SFA.Apprenticeships.Infrastructure.Repositories.Authentication.IoC;
     using StructureMap;
 
-    public class ActiveDirectoryUserDirectoryProviderTests
+    public class UserDirectoryProviderTests
     {
         private const string Password = "?Passw0rd14";
         private const string NewPassword = "?Passw0rd11";
@@ -18,7 +20,13 @@
         {
 #pragma warning disable 0618
             // TODO: AG: CRITICAL: NuGet package update on 2014-10-30.
-            ObjectFactory.Initialize(x => x.AddRegistry<UserDirectoryRegistry>());
+            ObjectFactory.Initialize(x =>
+            {
+                x.AddRegistry<CommonRegistry>();
+                x.AddRegistry<UserDirectoryRegistry>();
+                x.AddRegistry<AuthenticationRepositoryRegistry>();
+            });
+                                            
 #pragma warning restore 0618
 
             _service = ObjectFactory.GetInstance<IUserDirectoryProvider>();
