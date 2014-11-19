10e30759a0ed9b3b5eedb16b1db93708e24d73a4
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-11-19 10:27:31 +0000

    placeholders for queued processing of candidate activations

---
+++ src/SFA.Apprenticeships.Application.Interfaces/Messaging/CreateCandidateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.Apprenticeships.Application.Interfaces.Messaging
+{
+    using System;
+
+    public class CreateCandidateRequest
+    {
+        public Guid CandidateId { get; set; }
+    }
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.AsyncProcessor/Consumers/CreateCandidateRequestConsumerAsync.cs
@@ -0,0 +1,87 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.AsyncProcessor.Consumers
+{
+    using System.Threading.Tasks;
+    using Application.Interfaces.Messaging;
+    using Domain.Entities.Exceptions;
+    using Domain.Entities.Users;
+    using Domain.Interfaces.Messaging;
+    using Domain.Interfaces.Repositories;
+    using EasyNetQ.AutoSubscribe;
+    using NLog;
+
+    public class CreateCandidateRequestConsumerAsync : IConsumeAsync<CreateCandidateRequest>
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly ICandidateReadRepository _candidateReadRepository;
+        private readonly IUserReadRepository _userReadRepository;
+        private readonly IMessageBus _messageBus;
+
+        public CreateCandidateRequestConsumerAsync(
+            ICandidateReadRepository candidateReadRepository,
+            IUserReadRepository userReadRepository,
+            IMessageBus messageBus)
+        {
+            _candidateReadRepository = candidateReadRepository;
+            _userReadRepository = userReadRepository;
+            _messageBus = messageBus;
+        }
+
+        [AutoSubscriberConsumer(SubscriptionId = "CreateCandidateRequestConsumerAsync")]
+        public Task Consume(CreateCandidateRequest request)
+        {
+            Log("Received", request);
+
+            return Task.Run(() =>
+            {
+                Log("Submitting", request);
+
+                CreateCandidate(request);
+
+                Log("Submitted", request);
+            });
+        }
+
+        private void CreateCandidate(CreateCandidateRequest request)
+        {
+            var candidate = _candidateReadRepository.Get(request.CandidateId, true);
+            var user = _userReadRepository.Get(request.CandidateId);
+
+            //user.AssertState();
+            //EnsureApplicationCanBeCreated(candidate);
+
+            try
+            {
+                Log("Creating", request);
+
+                //candidate.LegacyApplicationId = _legacyApplicationProvider.CreateApplication(candidate);
+
+                Log("Created", request);
+
+                Log("Updating", request);
+
+                //candidate.SetStateSubmitted();
+                //_applicationWriteRepository.Save(candidate);
+
+                Log("Updated", request);
+            }
+            catch (CustomException ex)
+            {
+                //if (ex.Code != ErrorCodes.ApplicationDuplicatedError)
+                //{
+                //    // re-queue application for submission
+                //    var message = new CreateCandidateRequest
+                //    {
+                //        ApplicationId = request.ApplicationId
+                //    };
+                //    _messageBus.PublishMessage(message); 
+                //}
+            }
+        }
+
+        private static void Log(string narrative, CreateCandidateRequest request)
+        {
+            Logger.Debug("{0}: Candidate Id: \"{1}\"", narrative, request.CandidateId);
+        }
+    }
+}
