01dc031301de82d9ce9798fad416daae564b1df3
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2014-11-19 10:42:56 +0000

    removed activating state for user accounts

--- src/SFA.Apprenticeships.Domain.Entities/Users/UserStatuses.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Users/UserStatuses.cs
@@ -6,7 +6,6 @@
     {
         Unknown = 0,
         PendingActivation = 10,     // once registered and awaiting activation
-        Activating = 15,            // activation in process
         Active = 20,                // once activated
         Inactive = 30,              // when superseded by a new account (user changed their username)
         Locked = 90,                // if locked out for security reasons
