aeb822dddc4511d8bffdc4f538beab5a5ae436ac
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-11-21 09:28:11 +0000

    Switch on the new User Directory.

--- src/SFA.Apprenticeships.Infrastructure.UserDirectory/IoC/UserDirectoryRegistry.cs
+++ src/SFA.Apprenticeships.Infrastructure.UserDirectory/IoC/UserDirectoryRegistry.cs
@@ -1,20 +1,19 @@
 ﻿namespace SFA.Apprenticeships.Infrastructure.UserDirectory.IoC
 {
     using SFA.Apprenticeships.Application.Authentication;
-    using SFA.Apprenticeships.Infrastructure.UserDirectory.ActiveDirectory;
-    using SFA.Apprenticeships.Infrastructure.UserDirectory.Configuration;
+    using SFA.Apprenticeships.Infrastructure.UserDirectory.Hash;
     using StructureMap.Configuration.DSL;
 
     public class UserDirectoryRegistry : Registry
     {
         public UserDirectoryRegistry()
         {
-            For<ActiveDirectoryServer>().Use<ActiveDirectoryServer>();
-            For<ActiveDirectoryChangePassword>().Use<ActiveDirectoryChangePassword>();
-            For<ActiveDirectoryConfiguration>().Singleton().Use(ActiveDirectoryConfiguration.Instance);
-            For<IUserDirectoryProvider>().Use<ActiveDirectoryUserDirectoryProvider>();
-            //For<IUserDirectoryProvider>().Use<UserDirectoryProvider>();
-            //For<IPasswordHash>().Use<PasswordHash>();
+            //For<ActiveDirectoryServer>().Use<ActiveDirectoryServer>();
+            //For<ActiveDirectoryChangePassword>().Use<ActiveDirectoryChangePassword>();
+            //For<ActiveDirectoryConfiguration>().Singleton().Use(ActiveDirectoryConfiguration.Instance);
+            //For<IUserDirectoryProvider>().Use<ActiveDirectoryUserDirectoryProvider>();
+            For<IUserDirectoryProvider>().Use<UserDirectoryProvider>();
+            For<IPasswordHash>().Use<PasswordHash>();
         }
     }
 }
