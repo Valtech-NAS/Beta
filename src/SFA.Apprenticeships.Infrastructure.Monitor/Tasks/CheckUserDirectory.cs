2c1ee28e486216013c6b31c878676c331f691f2f
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-11-21 16:20:26 +0000

    Added a new monitor task to check the user directory.

---
+++ src/SFA.Apprenticeships.Infrastructure.Monitor/Tasks/CheckUserDirectory.cs
@@ -0,0 +1,33 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Monitor.Tasks
+{
+    using System;
+    using SFA.Apprenticeships.Application.Authentication;
+    using SFA.Apprenticeships.Domain.Entities.Exceptions;
+
+    public class CheckUserDirectory : IMonitorTask
+    {
+        private readonly IUserDirectoryProvider _userDirectoryProvider;
+
+        public CheckUserDirectory(IUserDirectoryProvider userDirectoryProvider)
+        {
+            _userDirectoryProvider = userDirectoryProvider;
+        }
+
+        public string TaskName
+        {
+            get { return "Check Active Directory"; }
+        }
+
+        public void Run()
+        {
+            try
+            {
+                _userDirectoryProvider.AuthenticateUser(Guid.NewGuid().ToString(), "valtech");
+            }
+            catch (CustomException)
+            {
+                // It's ok to get a custom exception
+            }
+        }
+    }
+}
