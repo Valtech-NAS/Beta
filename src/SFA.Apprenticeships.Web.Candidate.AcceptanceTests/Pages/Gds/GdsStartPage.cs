294121da148f2619bb07855645d2ab9362447b9f
alangorton <alan.gorton@valtech.co.uk>
2014-12-04 15:48:56 +0000

    GDS Start Page redirect: add a SpecBind test for Production only (tagged as @SmokeTestsProd)
    Conflicts:
    	src/SFA.Apprenticeships.Web.Candidate.AcceptanceTests/SFA.Apprenticeships.Web.Candidate.AcceptanceTests.csproj

---
+++ src/SFA.Apprenticeships.Web.Candidate.AcceptanceTests/Pages/Gds/GdsStartPage.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.AcceptanceTests.Pages.Gds
+{
+    using SpecBind.Pages;
+
+    [PageNavigation("https://www.gov.uk/findapprenticeship", IsAbsoluteUrl = true)]
+    [PageAlias("GdsStartPage")]
+    public class GdsStartPage
+    {
+    }
+}
