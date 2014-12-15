a6d5f523e853b3e0228915d2bda848c04acfc6bc
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-12-15 14:27:54 +0000

    Added old VacancyType field for backward compatibility

--- src/SFA.Apprenticeships.Domain.Entities/Vacancies/Apprenticeships/ApprenticeshipSummary.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Vacancies/Apprenticeships/ApprenticeshipSummary.cs
@@ -1,6 +1,13 @@
 ﻿namespace SFA.Apprenticeships.Domain.Entities.Vacancies.Apprenticeships
 {
+    using System;
+
     public class ApprenticeshipSummary : VacancySummary
     {
+        [Obsolete("Required for backward compatibility with previous document version that has this field.")]
+        public int? VacancyType {
+            get { return null; }
+            set { }
+        }
     }
 }
