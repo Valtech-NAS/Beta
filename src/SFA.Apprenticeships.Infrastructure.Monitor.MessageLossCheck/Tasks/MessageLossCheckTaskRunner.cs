1d775f35172066f1b8de61d62806199c2fd65c27
David Winchurch <david.winchurch@iterativeit.com>
2014-12-23 09:13:55 +0000

    Adding pause following fixing candidate records to allow queue messages to be processed

--- src/SFA.Apprenticeships.Infrastructure.Monitor.MessageLossCheck/Tasks/MessageLossCheckTaskRunner.cs
+++ src/SFA.Apprenticeships.Infrastructure.Monitor.MessageLossCheck/Tasks/MessageLossCheckTaskRunner.cs
@@ -1,10 +1,14 @@
 ﻿namespace SFA.Apprenticeships.Infrastructure.Monitor.MessageLossCheck.Tasks
 {
+    using System;
     using System.Collections.Generic;
+    using System.Threading;
     using Monitor.Tasks;
+    using NLog;
 
     public class MessageLossCheckTaskRunner : IMessageLossCheckTaskRunner
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         private readonly IEnumerable<IMonitorTask> _monitorTasks;
 
         public MessageLossCheckTaskRunner(IEnumerable<IMonitorTask> monitorTasks)
@@ -16,7 +20,13 @@
         {
             foreach (var monitorTask in _monitorTasks)
             {
+                Logger.Info("Running " + monitorTask.TaskName);
                 monitorTask.Run();
+                if (monitorTask.GetType() == typeof(CheckUnsentCandidateMessages))
+                {
+                    Logger.Info("Waiting for 5 minutes to allow any application queue messages to be processed after fixing the associated candidate");
+                    Thread.Sleep(TimeSpan.FromMinutes(5));
+                }
             }
         }
     }
