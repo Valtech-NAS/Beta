23c97a5e079d5aaefdf7b023c317c2c2bd0fc2bc
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-01-05 10:22:55 +0000

    comment update

--- src/SFA.Apprenticeships.Application.Interfaces/Messaging/CandidateMessageTypes.cs
+++ src/SFA.Apprenticeships.Application.Interfaces/Messaging/CandidateMessageTypes.cs
@@ -1,6 +1,5 @@
 ﻿namespace SFA.Apprenticeships.Application.Interfaces.Messaging
 {
-    //todo: shouldn't be in this project - this is process specific so should be in the application layer
     public enum CandidateMessageTypes
     {
         SendActivationCode,
