a4479b17269025402b8ec6a3c4f2bcf3b375fe70
Henry Charge <henry.charge@valtech.co.uk>
2015-01-07 09:45:24 +0000

    Updated custom scripts

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/scripts.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/scripts.js
@@ -358,4 +358,33 @@ $(function() {
     }
   });
 
+});;$(function () {
+
+  var sortControl = $('.float-right-wrap');
+  var secondToLast = $('.search-results__item:nth-last-child(2)');
+  var origPosition = $('.fixedsticky').css('top');
+  var posNumber = parseInt(origPosition, 10);
+
+  $(window).scroll(function() {
+    var sort = sortControl[0];
+    var second = secondToLast[0];
+
+    var sortTopPos = sort.getBoundingClientRect().top;
+    var secLastTopPos = second.getBoundingClientRect().top;
+
+
+    if(sortTopPos < posNumber) {
+      $('.fixedsticky').addClass('sticky');
+    } else {
+      $('.fixedsticky').removeClass('sticky');
+    }
+
+    if(secLastTopPos < 0) {
+      $('.fixedsticky').css('top', secLastTopPos + posNumber + 'px');
+    } else {
+      $('.fixedsticky').css('top', origPosition);
+    }
+  });
+
 });
+
