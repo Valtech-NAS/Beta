401ec924f532f11f10a327b9d335a5d1aa271b5b
Henry Charge <henry.charge@valtech.co.uk>
2015-01-08 17:19:36 +0000

    Update sticky script to only run if element exists

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/scripts.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/scripts.js
@@ -359,30 +359,39 @@ $(function() {
   });
 
 });;$(function () {
+  if($('.fixedsticky').length > 0) {
 
-  var sortControl = $('.float-right-wrap');
-  var secondToLast = $('.search-results__item:nth-last-child(2)');
-  var origPosition = '23px';
-  var posNumber = 23;
+    var sortControl = $('.float-right-wrap');
+    var secondToLast = $('.search-results__item:nth-last-child(2)');
+    var origPosition = '23px';
+    var posNumber = 23;
 
-  $(window).scroll(function() {
-    var sortOffset = sortControl.offset(),
-        secondOffset = secondToLast.offset(),
-        sortTopPos = sortOffset.top - $(window).scrollTop(),
-        secLastTopPos = secondOffset.top - $(window).scrollTop();
+    stickySidebar();
 
-    if(sortTopPos < posNumber) {
-      $('.fixedsticky').addClass('sticky');
-    } else {
-      $('.fixedsticky').removeClass('sticky');
+    $(window).scroll(stickySidebar);
+
+    $(window).resize(stickySidebar);
+
+    function stickySidebar() {
+      var sortOffset = sortControl.offset(),
+          secondOffset = secondToLast.offset(),
+          sortTopPos = sortOffset.top - $(window).scrollTop(),
+          secLastTopPos = secondOffset.top - $(window).scrollTop();
+
+      if(sortTopPos < posNumber) {
+        $('.fixedsticky').addClass('sticky');
+      } else {
+        $('.fixedsticky').removeClass('sticky');
+      }
+
+      if(secLastTopPos < 0) {
+        $('.fixedsticky').css('top', secLastTopPos + posNumber + 'px');
+      } else {
+        $('.fixedsticky').css('top', origPosition);
+      }
     }
+  }
 
-    if(secLastTopPos < 0) {
-      $('.fixedsticky').css('top', secLastTopPos + posNumber + 'px');
-    } else {
-      $('.fixedsticky').css('top', origPosition);
-    }
-  });
 
 });
 
