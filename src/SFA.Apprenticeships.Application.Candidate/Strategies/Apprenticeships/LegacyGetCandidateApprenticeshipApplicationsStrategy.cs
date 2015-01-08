e00a2e01b90bf77e1132be4a1d9e913d021e6e1a
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-01-08 10:53:48 +0000

    corrected namespace and code tidy/comment removal

--- src/SFA.Apprenticeships.Application.Candidate/Strategies/Apprenticeships/LegacyGetCandidateApprenticeshipApplicationsStrategy.cs
+++ src/SFA.Apprenticeships.Application.Candidate/Strategies/Apprenticeships/LegacyGetCandidateApprenticeshipApplicationsStrategy.cs
@@ -1,10 +1,8 @@
-﻿namespace SFA.Apprenticeships.Application.Candidate.Strategies
+﻿namespace SFA.Apprenticeships.Application.Candidate.Strategies.Apprenticeships
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using ApplicationUpdate;
-    using Apprenticeships;
     using Domain.Entities.Applications;
     using Domain.Interfaces.Repositories;
     using NLog;
@@ -32,37 +30,13 @@
 
         public IList<ApprenticeshipApplicationSummary> GetApplications(Guid candidateId)
         {
-            // try to get the latest status of apps for the specified candidate from legacy
             try
             {
+                // try to get the latest status of apps for the specified candidate from legacy
                 var candidate = _candidateReadRepository.Get(candidateId);
+                var submittedApplicationStatuses = _legacyApplicationStatusesProvider.GetCandidateApplicationStatuses(candidate);
 
-                //todo: may optionally skip 1+2 below if recently retrieved... TBC later
-                // (1) get the current application statuses for submitted applications from legacy
-                var submittedApplicationStatuses = _legacyApplicationStatusesProvider.GetCandidateApplicationStatuses(candidate).ToList();
-
-                // (2) update the candidate's applications in the repo with statuses from (1)
                 _applicationStatusUpdater.Update(candidate, submittedApplicationStatuses);
-
-                //todo: not doing 3 below for now... TBC later
-                // (3) for any draft applications, update status based on current vacancy status
-                //var candidateApplications = _apprenticeshipApplicationReadRepository.GetForCandidate(candidateId);
-                //var draftApplicationIds = candidateApplications
-                //    .Where(a => a.Status == ApplicationStatuses.Draft)
-                //    .Select(a => a.ApplicationId);
-
-                //foreach (var applicationId in draftApplicationIds) //todo: parallel?
-                //{
-                //    var application = _apprenticeshipApplicationReadRepository.Get(applicationId);
-                //    var vacancyStatus = _vacancyStatusProvider.GetVacancyStatus(application.LegacyApplicationId);
-
-                //    //todo: update each application status for any that are NOT VacancyStatuses.Live
-                //    //if (blah)
-                //    //{
-                //    //    application.Status = ApplicationStatuses.Expired;
-                //    //    _applicationWriteRepository.Save(application);
-                //    //}
-                //}
             }
             catch (Exception ex)
             {
