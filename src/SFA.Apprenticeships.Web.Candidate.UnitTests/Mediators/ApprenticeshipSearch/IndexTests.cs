694696bc63b7f595b4fca2f8559cb23819622cf4
David Winchurch <david.winchurch@iterativeit.com>
2015-01-12 14:05:27 +0000

    Refactor of apprenticeship search mediator using tests

--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipSearch/IndexTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipSearch/IndexTests.cs
@@ -1,39 +1,18 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.ApprenticeshipSearch
 {
     using Candidate.Mediators;
-    using Candidate.Providers;
     using Common.Constants;
-    using Common.Providers;
     using Domain.Entities.Vacancies.Apprenticeships;
-    using Domain.Interfaces.Configuration;
     using FluentAssertions;
-    using Moq;
     using NUnit.Framework;
 
     [TestFixture]
     public class IndexTests : TestsBase
     {
-        private Mock<IConfigurationManager> _configurationManager;
-        private Mock<ISearchProvider> _searchProvider;
-        private Mock<IApprenticeshipVacancyDetailProvider> _apprenticeshipVacancyDetailProvider;
-        private Mock<IUserDataProvider> _userDataProvider;
-        private IApprenticeshipSearchMediator _mediator;
-
-        [SetUp]
-        public void Setup()
-        {
-            _configurationManager = new Mock<IConfigurationManager>();
-            _configurationManager.Setup(cm => cm.GetAppSetting<int>("VacancyResultsPerPage")).Returns(5);
-            _searchProvider = new Mock<ISearchProvider>();
-            _apprenticeshipVacancyDetailProvider = new Mock<IApprenticeshipVacancyDetailProvider>();
-            _userDataProvider = new Mock<IUserDataProvider>();
-            _mediator = GetMediator(_configurationManager.Object, _searchProvider.Object, _apprenticeshipVacancyDetailProvider.Object, _userDataProvider.Object);
-        }
-
         [Test]
         public void Ok()
         {
-            var response = _mediator.Index();
+            var response = Mediator.Index();
 
             response.AssertCode(Codes.ApprenticeshipSearch.Index.Ok, true);
 
@@ -48,9 +27,9 @@
         [Test]
         public void RememberApprenticeshipLevel()
         {
-            _userDataProvider.Setup(udp => udp.Get(UserDataItemNames.ApprenticeshipLevel)).Returns("Advanced");
+            UserDataProvider.Setup(udp => udp.Get(UserDataItemNames.ApprenticeshipLevel)).Returns("Advanced");
 
-            var response = _mediator.Index();
+            var response = Mediator.Index();
 
             var viewModel = response.ViewModel;
             viewModel.ApprenticeshipLevel.Should().Be("Advanced");
