bea21becf2818268e31487a20d874b699d9b3122
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-01-12 12:58:50 +0000

    Added unit tests to Register mediator

---
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Register/ActivateTests.cs
@@ -0,0 +1,92 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.Register
+{
+    using System;
+    using Moq;
+    using NUnit.Framework;
+    using SFA.Apprenticeships.Web.Candidate.Constants.Pages;
+    using SFA.Apprenticeships.Web.Candidate.Mediators;
+    using SFA.Apprenticeships.Web.Candidate.ViewModels.Register;
+    using SFA.Apprenticeships.Web.Common.Constants;
+
+    [TestFixture]
+    public class ActivateTests : RegisterBaseTests
+    {
+        private const string SomeErrorMessage = "SomeErrorMessage";
+        private const string ValidActivationCode = "ABC123";
+        private const string ValidEmailAddress = "validEmailAddress@gmail.com";
+        private const string InvalidActivationCode = "invalidActivationCode";
+
+        [Test]
+        public void ValidationError()
+        {
+            var activationViewModel = new ActivationViewModel
+            {
+                ActivationCode = InvalidActivationCode
+            };
+            var response = _registerMediator.Activate(Guid.NewGuid(), activationViewModel);
+
+            response.AssertValidationResult(Codes.RegisterMediatorCodes.Activate.FailedValidation, true);
+        }
+
+        [Test]
+        public void UserActivated()
+        {
+            var activationViewModel = new ActivationViewModel
+            {
+                ActivationCode = ValidActivationCode,
+                EmailAddress = ValidEmailAddress
+            };
+
+            _candidateServiceProvider.Setup(csp => csp.Activate(It.IsAny<ActivationViewModel>(), It.IsAny<Guid>()))
+                .Returns(new ActivationViewModel
+                {
+                    State = ActivateUserState.Activated
+                });
+
+            var response = _registerMediator.Activate(Guid.NewGuid(), activationViewModel);
+
+            response.AssertMessage(Codes.RegisterMediatorCodes.Activate.SuccessfullyActivated, ActivationPageMessages.AccountActivated, UserMessageLevel.Success, true);
+        }
+
+        [Test]
+        public void ActivationError()
+        {
+            var activationViewModel = new ActivationViewModel
+            {
+                ActivationCode = ValidActivationCode,
+                EmailAddress = ValidEmailAddress
+            };
+
+            _candidateServiceProvider.Setup(csp => csp.Activate(It.IsAny<ActivationViewModel>(), It.IsAny<Guid>()))
+                .Returns(new ActivationViewModel
+                {
+                    State = ActivateUserState.Error,
+                    ViewModelMessage = SomeErrorMessage
+                });
+
+            var response = _registerMediator.Activate(Guid.NewGuid(), activationViewModel);
+
+            response.AssertMessage(Codes.RegisterMediatorCodes.Activate.SuccessfullyActivated, SomeErrorMessage, UserMessageLevel.Success, true);
+        }
+
+        [Test]
+        public void InvalidCode()
+        {
+            var activationViewModel = new ActivationViewModel
+            {
+                ActivationCode = ValidActivationCode,
+                EmailAddress = ValidEmailAddress
+            };
+
+            _candidateServiceProvider.Setup(csp => csp.Activate(It.IsAny<ActivationViewModel>(), It.IsAny<Guid>()))
+                .Returns(new ActivationViewModel
+                {
+                    State = ActivateUserState.InvalidCode
+                });
+
+            var response = _registerMediator.Activate(Guid.NewGuid(), activationViewModel);
+
+            response.AssertValidationResult(Codes.RegisterMediatorCodes.Activate.InvalidActivationCode, true);
+        }
+    }
+}
--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Register/ResetPasswordTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Register/ResetPasswordTests.cs
@@ -1,9 +1,109 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.Register
 {
+    using Moq;
     using NUnit.Framework;
+    using SFA.Apprenticeships.Domain.Entities.Users;
+    using SFA.Apprenticeships.Web.Candidate.Constants.Pages;
+    using SFA.Apprenticeships.Web.Candidate.Mediators;
+    using SFA.Apprenticeships.Web.Candidate.ViewModels.Register;
+    using SFA.Apprenticeships.Web.Common.Constants;
 
     [TestFixture]
     public class ResetPasswordTests : RegisterBaseTests
     {
+        private const string ValidPassword = "?Password01!";
+        private const string ValidPasswordResetCode = "ABC123";
+        private const string VaildEmailAddress = "validEmailAddress@gmail.com";
+        private const string InvalidPasswordResetCode = "invalidPasswordResetCode";
+        private const string ErrorMessage = "Some error message";
+
+        [Test]
+        public void ValidationFailure()
+        {
+            var resetPasswordViewModel = new PasswordResetViewModel
+            {
+                PasswordResetCode = InvalidPasswordResetCode
+            };
+
+            var response = _registerMediator.ResetPassword(resetPasswordViewModel);
+
+            response.AssertValidationResult(Codes.RegisterMediatorCodes.ResetPassword.FailedValidation, true);
+        }
+
+        [Test]
+        public void ErrorVerifyingPassword()
+        {
+            var resetPasswordViewModel = GetValidPasswordResetViewModel();
+
+            _candidateServiceProvider.Setup(csp => csp.VerifyPasswordReset(It.IsAny<PasswordResetViewModel>()))
+                .Returns(new PasswordResetViewModel
+                {
+                    ViewModelMessage = ErrorMessage
+                });
+
+            var response = _registerMediator.ResetPassword(resetPasswordViewModel);
+
+            response.AssertMessage(Codes.RegisterMediatorCodes.ResetPassword.FailedToResetPassword, ErrorMessage, UserMessageLevel.Warning, true);
+        }
+
+        [Test]
+        public void UserLocked()
+        {
+            var resetPasswordViewModel = GetValidPasswordResetViewModel();
+
+            _candidateServiceProvider.Setup(csp => csp.VerifyPasswordReset(It.IsAny<PasswordResetViewModel>()))
+                .Returns(new PasswordResetViewModel
+                {
+                    UserStatus = UserStatuses.Locked
+                });
+
+            var response = _registerMediator.ResetPassword(resetPasswordViewModel);
+
+            response.AssertCode(Codes.RegisterMediatorCodes.ResetPassword.UserAccountLocked, true);
+        }
+
+        [Test]
+        public void InvalidResetCode()
+        {
+            var resetPasswordViewModel = GetValidPasswordResetViewModel();
+
+            _candidateServiceProvider.Setup(csp => csp.VerifyPasswordReset(It.IsAny<PasswordResetViewModel>()))
+                .Returns(new PasswordResetViewModel
+                {
+                    IsPasswordResetCodeValid = false
+                });
+
+            var response = _registerMediator.ResetPassword(resetPasswordViewModel);
+
+            response.AssertCode(Codes.RegisterMediatorCodes.ResetPassword.InvalidResetCode, true);
+        }
+
+        [Test]
+        public void SuccessfullResetPassword()
+        {
+            var resetPasswordViewModel = GetValidPasswordResetViewModel();
+
+            _candidateServiceProvider.Setup(csp => csp.VerifyPasswordReset(It.IsAny<PasswordResetViewModel>()))
+                .Returns(new PasswordResetViewModel
+                {
+                    IsPasswordResetCodeValid = true
+                });
+
+            var response = _registerMediator.ResetPassword(resetPasswordViewModel);
+
+            response.AssertMessage(Codes.RegisterMediatorCodes.ResetPassword.SuccessfullyResetPassword,PasswordResetPageMessages.SuccessfulPasswordReset, UserMessageLevel.Success, true);
+        }
+
+        private static PasswordResetViewModel GetValidPasswordResetViewModel()
+        {
+            var resetPasswordViewModel = new PasswordResetViewModel
+            {
+                PasswordResetCode = ValidPasswordResetCode,
+                EmailAddress = VaildEmailAddress,
+                Password = ValidPassword,
+                ConfirmPassword = ValidPassword
+            };
+            return resetPasswordViewModel;
+        }
     }
 }
