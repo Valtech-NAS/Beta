cdd382dd1285052405710248d4ee35cbbd733652
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-01-14 11:10:47 +0000

    Added tests to AddEmptyQualificationRows and AddEmptyWorkExperienceRows

--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipApplication/AddEmptyQualificationRowsTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipApplication/AddEmptyQualificationRowsTests.cs
@@ -1,14 +1,23 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.ApprenticeshipApplication
 {
+    using System.Collections.Generic;
     using Candidate.Mediators;
     using Candidate.ViewModels.Applications;
     using Candidate.ViewModels.Candidate;
     using Candidate.ViewModels.VacancySearch;
+    using FluentAssertions;
     using NUnit.Framework;
 
     [TestFixture]
     public class AddEmptyQualificationRowsTests : TestsBase
     {
+        private const bool SomeBoolean = false;
+        private const string SomeYear = "2011";
+        private const string BlankSpace = "  ";
+        private const string SomeGrade = "A";
+        private const string SomeQualificationType = "QualificationType";
+        private const string SomeSubject = "Subject";
+
         [Test]
         public void Ok()
         {
@@ -21,6 +30,48 @@
             var response = Mediator.AddEmptyQualificationRows(viewModel);
 
             response.AssertCode(Codes.ApprenticeshipApplication.AddEmptyQualificationRows.Ok, true);
+        }
+
+        [Test]
+        public void WillRemoveEmptyQualficationRows()
+        {
+            var viewModel = new ApprenticeshipApplicationViewModel
+            {
+                Candidate = CreateCandidateWithOneQualificationAndTwoEmptyQualifications(),
+                VacancyDetail = new VacancyDetailViewModel()
+            };
+
+            var response = Mediator.AddEmptyQualificationRows(viewModel);
+
+            response.AssertCode(Codes.ApprenticeshipApplication.AddEmptyQualificationRows.Ok, true);
+            response.ViewModel.Candidate.Qualifications.Should().HaveCount(1);
+        }
+
+        private static ApprenticeshipCandidateViewModel CreateCandidateWithOneQualificationAndTwoEmptyQualifications()
+        {
+            return new ApprenticeshipCandidateViewModel
+            {
+                Qualifications = new List<QualificationsViewModel>
+                {
+                    new QualificationsViewModel(),
+                    new QualificationsViewModel
+                    {
+                        Grade = SomeGrade,
+                        IsPredicted = SomeBoolean,
+                        QualificationType = SomeQualificationType,
+                        Subject = SomeSubject,
+                        Year = SomeYear
+                    },
+                    new QualificationsViewModel
+                    {
+                        Grade = BlankSpace,
+                        IsPredicted = SomeBoolean,
+                        QualificationType = BlankSpace,
+                        Subject = BlankSpace,
+                        Year = BlankSpace
+                    }
+                }
+            };
         }
     }
 }
--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipApplication/AddEmptyWorkExperienceRowsTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/ApprenticeshipApplication/AddEmptyWorkExperienceRowsTests.cs
@@ -1,14 +1,23 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Mediators.ApprenticeshipApplication
 {
+    using System.Collections.Generic;
     using Candidate.Mediators;
     using Candidate.ViewModels.Applications;
     using Candidate.ViewModels.Candidate;
     using Candidate.ViewModels.VacancySearch;
+    using FluentAssertions;
     using NUnit.Framework;
 
     [TestFixture]
     public class AddEmptyWorkExperienceRowsTests : TestsBase
     {
+        private static string SomeJobTitle;
+        private const string BlankSpace = "  ";
+        private const string SomeWorkExperienceDescription = "Work experience description";
+        private const string SomeEmployer = "Some employer";
+        private const int SomeMonth = 1;
+        private const string SomeYear = "2012";
+
         [Test]
         public void Ok()
         {
@@ -21,6 +30,53 @@
             var response = Mediator.AddEmptyWorkExperienceRows(viewModel);
 
             response.AssertCode(Codes.ApprenticeshipApplication.AddEmptyWorkExperienceRows.Ok, true);
+        }
+
+        [Test]
+        public void WillRemoveEmptyWorkExperienceRows()
+        {
+            var viewModel = new ApprenticeshipApplicationViewModel
+            {
+                Candidate = CreateCandidateWithOneWorkExerienceAndTwoEmptyWorkExperiences(),
+                VacancyDetail = new VacancyDetailViewModel()
+            };
+
+            var response = Mediator.AddEmptyWorkExperienceRows(viewModel);
+
+            response.AssertCode(Codes.ApprenticeshipApplication.AddEmptyWorkExperienceRows.Ok, true);
+            response.ViewModel.Candidate.WorkExperience.Should().HaveCount(1);
+        }
+
+        private static ApprenticeshipCandidateViewModel CreateCandidateWithOneWorkExerienceAndTwoEmptyWorkExperiences()
+        {
+            SomeJobTitle = "Job title";
+            return new ApprenticeshipCandidateViewModel
+            {
+                WorkExperience = new List<WorkExperienceViewModel>
+                {
+                    new WorkExperienceViewModel(),
+                    new WorkExperienceViewModel
+                    {
+                        Description = SomeWorkExperienceDescription,
+                        Employer = SomeEmployer,
+                        FromMonth = SomeMonth,
+                        FromYear = SomeYear,
+                        JobTitle = SomeJobTitle,
+                        ToMonth = SomeMonth,
+                        ToYear = SomeYear
+                    },
+                    new WorkExperienceViewModel
+                    {
+                        Description = BlankSpace,
+                        Employer = BlankSpace,
+                        FromMonth = SomeMonth,
+                        FromYear = BlankSpace,
+                        JobTitle = BlankSpace,
+                        ToMonth = SomeMonth,
+                        ToYear = BlankSpace
+                    }
+                }
+            };
         }
     }
 }
