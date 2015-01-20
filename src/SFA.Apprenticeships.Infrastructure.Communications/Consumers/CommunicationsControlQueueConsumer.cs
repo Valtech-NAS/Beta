8aca2893e64f2be302a641731725a535fd51d4b5
David Winchurch <david.winchurch@iterativeit.com>
2015-01-20 10:54:56 +0000

    New Communications worker role

---
+++ src/SFA.Apprenticeships.Infrastructure.Communications/Consumers/CommunicationsControlQueueConsumer.cs
@@ -0,0 +1,30 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Communications.Consumers
+{
+    using System.Threading.Tasks;
+    using Application.Communications;
+    using Azure.Common.Messaging;
+    using Domain.Interfaces.Messaging;
+
+    public class CommunicationsControlQueueConsumer : AzureControlQueueConsumer
+    {
+        private readonly ICommunicationProcessor _communicationProcessor;
+
+        public CommunicationsControlQueueConsumer(IProcessControlQueue<StorageQueueMessage> messageService, ICommunicationProcessor communicationProcessor) : base(messageService, "Communications")
+        {
+            _communicationProcessor = communicationProcessor;
+        }
+
+        public Task CheckScheduleQueue()
+        {
+            return Task.Run(() =>
+            {
+                var scheduleerNotification = GetLatestQueueMessage();
+                if (scheduleerNotification != null)
+                {
+                    _communicationProcessor.SendDailyDigests();
+                    _messageService.DeleteMessage(scheduleerNotification.MessageId, scheduleerNotification.PopReceipt);
+                }
+            });
+        }
+    }
+}
---
+++ src/SFA.Apprenticeships.Infrastructure.Communications/IoC/CommunicationsRegistry.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.Apprenticeships.Infrastructure.Communications.IoC
+{
+    using Consumers;
+    using StructureMap.Configuration.DSL;
+
+    public class CommunicationsRegistry : Registry
+    {
+        public CommunicationsRegistry()
+        {
+            For<CommunicationsControlQueueConsumer>().Use<CommunicationsControlQueueConsumer>();
+        }
+    }
+}
