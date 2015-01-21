0338bb80b8d5f76590f47f110b74a23a4f2f9655
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-01-21 10:55:30 +0000

    Changed email validation regular expression to be more robust.

--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Register/RegisterTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Mediators/Register/RegisterTests.cs
@@ -96,6 +96,58 @@
             response.AssertValidationResult(Codes.RegisterMediatorCodes.Register.ValidationFailed, true);
         }
 
+        [TestCase("email@domain.com", true)]
+        [TestCase("firstname.lastname@domain.com", true)]
+        [TestCase("email@subdomain.domain.com", true)]
+        [TestCase("firstname+lastname@domain.com", true)]
+        // [TestCase("email@123.123.123.123", true)] The regex doesn't support this scenario
+        // [TestCase("email@[123.123.123.123]", true)] The regex doesn't support this scenario
+        [TestCase("\"email\"@domain.com", true)]
+        [TestCase("1234567890@domain.com", true)]
+        [TestCase("email@domain-one.com", true)]
+        [TestCase("_______@domain.com", true)]
+        [TestCase("email@domain.name", true)]
+        [TestCase("email@domain.co.jp", true)]
+        [TestCase("emailAddress@gmail.com", true)]
+        [TestCase("firstname-lastname@domain.com", true)]
+        [TestCase("plainaddress", false)]
+        [TestCase("#@%^%#$@#$@#.com", false)]
+        // [TestCase("@domain.com", false)] The regex doesn't support this scenario
+        [TestCase("Joe Smith <email@domain.com>", false)]
+        [TestCase("email.domain.com", false)]
+        [TestCase("email@domain@domain.com", false)]
+        [TestCase(".email@domain.com", false)]
+        [TestCase("email.@domain.com", false)]
+        [TestCase("email..email@domain.com", false)]
+        [TestCase("あいうえお@domain.com", false)]
+        [TestCase("email@domain.com (Joe Smith)", false)]
+        [TestCase("email@domain", false)]
+        [TestCase("email@-domain.com", false)]
+        // [TestCase("email@domain.web", false)] The regex doesn't support this scenario
+        [TestCase("email@111.222.333.44444", false)]
+        [TestCase("email@domain..com", false)]
+        public void ValidateEmail(string email, bool isValid)
+        {
+            _candidateServiceProvider.Setup(x => x.IsUsernameAvailable(It.IsAny<string>()))
+                .Returns(new UserNameAvailability { IsUserNameAvailable = true });
+
+            _candidateServiceProvider.Setup(csp => csp.Register(It.IsAny<RegisterViewModel>())).Returns(true);
+
+            var registerViewModel = new RegisterViewModel
+            {
+                EmailAddress = email,
+                Password = ValidPassword,
+                ConfirmPassword = ValidPassword,
+                Firstname = SomeName,
+                Lastname = SomeName,
+                HasAcceptedTermsAndConditions = true,
+                PhoneNumber = SomePhoneNumber
+            };
+            var response = _registerMediator.Register(registerViewModel);
+
+            Assert.AreEqual(isValid, response.Code == "SuccessfullyRegistered");
+        }
+
         [Test]
         public void SuccessfullyRegistered()
         {
