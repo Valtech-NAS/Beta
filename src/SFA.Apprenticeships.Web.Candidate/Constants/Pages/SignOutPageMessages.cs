9abca0406857ac0ad62b4ef36f89019cc7581c3b
Krister Bone <krister.bone@valtech.co.uk>
2015-01-23 15:36:05 +0000

    Added TODOs to Ts & Cs content

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/SignOutPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/SignOutPageMessages.cs
@@ -4,6 +4,6 @@
     {
         public const string SignOutMessageText = "You've successfully signed out";
         public const string SessionTimeoutMessageText = "You've been automatically signed out due to inactivity";
-        public const string MustAcceptUpdatedTermsAndConditions = "You must accept our updated terms and conditions to use thie service.";
+        public const string MustAcceptUpdatedTermsAndConditions = "You must accept our updated terms and conditions to use thie service.<br/>TODO: Header text needs reviewed and link to search added";
     }
 }
