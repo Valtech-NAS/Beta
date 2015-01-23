fbeb325f8873a3eea44671c51f985325128e50b3
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-01-23 11:27:00 +0000

    removed serialization assembly ref

--- src/SFA.Apprenticeships.Application.Interfaces/Search/SearchResults.cs
+++ src/SFA.Apprenticeships.Application.Interfaces/Search/SearchResults.cs
@@ -1,10 +1,7 @@
 ﻿namespace SFA.Apprenticeships.Application.Interfaces.Search
 {
     using System.Collections.Generic;
-    using System.Runtime.Serialization;
 
-    //todo: why is this marked up as a WCF contract? 
-    [DataContract(Namespace = "http://candidates.gov.uk")]
     public class SearchResults<TResult> where TResult : class
     {
         public SearchResults(long total, int pageNumber, IEnumerable<TResult> results)
@@ -14,13 +11,10 @@
             Results = results;
         }
 
-        [DataMember(Order = 1, Name = "Total")]
         public long Total { get; private set; }
 
-        [DataMember(Order = 2, Name = "PageNumber")]
         public int PageNumber { get; private set; }
 
-        [DataMember(Order = 3, Name = "Results")]
         public IEnumerable<TResult> Results { get; private set; }
     }
 }
