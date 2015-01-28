2f2eb9686b43c996a3e8f34b7a1effcf1ac874dc
alangorton <alan.gorton@valtech.co.uk>
2015-01-28 16:10:43 +0000

    Added missing folder.

---
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Views/Dashboard/DashboardTests.cs
@@ -0,0 +1,36 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Views.Dashboard
+{
+    using System.Web.Routing;
+    using Candidate.ViewModels.Applications;
+    using Candidate.ViewModels.MyApplications;
+    using Candidate.Views.Account;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RazorGenerator.Testing;
+
+    [TestFixture]
+    public class DashboardTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            RouteTable.Routes.Clear();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        [Test]
+        public void ShouldShowBigFindApprenticeshipButtonWhenNoApplications()
+        {
+            var index = new Index();
+
+            var maavm = new MyApprenticeshipApplicationViewModel[] {};
+            var mtavm = new MyTraineeshipApplicationViewModel[] {};
+            var tfvm = new TraineeshipFeatureViewModel();
+
+            var vm = new MyApplicationsViewModel(maavm, mtavm, tfvm);
+            var view = index.RenderAsHtml(vm);
+
+            view.GetElementbyId("find-apprenticeship-button").Should().NotBeNull();
+        }
+    }
+}
