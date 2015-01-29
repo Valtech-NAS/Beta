0bd21259359a73edf3f5169afa10249ea504cb9f
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-01-29 13:17:32 +0000

    added exception data and nlog config to write error codes to log

--- src/SFA.Apprenticeships.Domain.Entities/Exceptions/CustomException.cs
+++ src/SFA.Apprenticeships.Domain.Entities/Exceptions/CustomException.cs
@@ -2,37 +2,47 @@
 {
     using System;
 
-    public class CustomException : Exception
+    public sealed class CustomException : Exception
     {
         public CustomException(string code)
         {
+            StoreData(code);
             Code = code;
         }
 
         public CustomException(string message, string code)
             : base(message)
         {
+            StoreData(code);
             Code = code;
         }
 
         public CustomException(string format, string code, params object[] args)
             : base(string.Format(format, args))
         {
+            StoreData(code);
             Code = code;
         }
 
         public CustomException(string message, Exception innerException, string code)
             : base(message, innerException)
         {
+            StoreData(code);
             Code = code;
         }
 
         public CustomException(string format, Exception innerException, string code, params object[] args)
             : base(string.Format(format, args), innerException)
         {
+            StoreData(code);
             Code = code;
         }
 
         public string Code { get; private set; }
+
+        private void StoreData(string code)
+        {
+            Data.Add("error_code", code);
+        }
     }
 }
