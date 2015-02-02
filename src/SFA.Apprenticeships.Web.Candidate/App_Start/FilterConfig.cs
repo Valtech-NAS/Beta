2618484ba8388b9ee78c463050a68b8ac42194b5
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-02-02 15:04:52 +0000

    Removed deprecated ObjectFactory.Instance on web project.

--- src/SFA.Apprenticeships.Web.Candidate/App_Start/FilterConfig.cs
+++ src/SFA.Apprenticeships.Web.Candidate/App_Start/FilterConfig.cs
@@ -3,14 +3,15 @@
     using System.Linq;
     using System.Web.Mvc;
     using Common.IoC;
+    using StructureMap;
 
     public class FilterConfig
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters, IContainer container)
         {
             var oldProvider = FilterProviders.Providers.Single(f => f is FilterAttributeFilterProvider);
             FilterProviders.Providers.Remove(oldProvider);
-            FilterProviders.Providers.Add(new DependencyResolverFilterProvider());
+            FilterProviders.Providers.Add(new DependencyResolverFilterProvider(container));
         }
     }
 }
