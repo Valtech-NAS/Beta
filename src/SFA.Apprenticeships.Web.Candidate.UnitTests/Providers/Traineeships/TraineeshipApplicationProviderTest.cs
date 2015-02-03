5cfc46c1b25bcf4429d68163e3e85b825ee17877
Mark Gwilliam <mark.gwilliam@valtech.co.uk>
2015-02-03 09:15:48 +0000

    fixed unit tests that needed mock logger

--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Providers/Traineeships/TraineeshipApplicationProviderTest.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Providers/Traineeships/TraineeshipApplicationProviderTest.cs
@@ -1,13 +1,14 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Providers.Traineeships
 {
     using System;
+    using Application.Interfaces.Logging;
     using FluentAssertions;
     using Moq;
     using NUnit.Framework;
-    using SFA.Apprenticeships.Application.Interfaces.Candidates;
-    using SFA.Apprenticeships.Domain.Entities.Applications;
-    using SFA.Apprenticeships.Domain.Interfaces.Mapping;
-    using SFA.Apprenticeships.Web.Candidate.Providers;
+    using Application.Interfaces.Candidates;
+    using Domain.Entities.Applications;
+    using Domain.Interfaces.Mapping;
+    using Candidate.Providers;
 
     [TestFixture]
     public class TraineeshipApplicationProviderTest
@@ -15,12 +16,13 @@
         [Test]
         public void WhenIGetTheApplicationViewModel_IfIveAlreadyAppliedForTheApprenticeship_IGetAViewModelWithError()
         {
+            var logger = new Mock<ILogService>();
             var mapper = new Mock<IMapper>();
             var candidateService = new Mock<ICandidateService>();
             var traineeshipVacancyDetailProvider = new Mock<ITraineeshipVacancyDetailProvider>();
 
             var traineeshipApplicationProvider = new TraineeshipApplicationProvider(
-                mapper.Object, candidateService.Object, traineeshipVacancyDetailProvider.Object);
+                mapper.Object, candidateService.Object, traineeshipVacancyDetailProvider.Object, logger.Object);
 
             candidateService.Setup(cs => cs.GetTraineeshipApplication(It.IsAny<Guid>(), It.IsAny<int>()))
                 .Returns(new TraineeshipApplicationDetail());
