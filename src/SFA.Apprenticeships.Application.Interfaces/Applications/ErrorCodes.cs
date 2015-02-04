528af2281d7cff7b4ffebbfdbe54ba76a449e44f
David Winchurch <david.winchurch@iterativeit.com>
2015-02-04 10:04:09 +0000

    Used meaningful strings for application error codes

--- src/SFA.Apprenticeships.Application.Interfaces/Applications/ErrorCodes.cs
+++ src/SFA.Apprenticeships.Application.Interfaces/Applications/ErrorCodes.cs
@@ -2,13 +2,12 @@
 {
     public static class ErrorCodes
     {
-        //todo: use meaningful strings here!
-        public const string ApplicationNotFoundError = "Application001";
-        public const string ApplicationInIncorrectStateError = "Application002";
-        public const string ApplicationViewIdNotFound = "Application004";
-        public const string ApplicationCreationError = "Application005";
-        public const string ApplicationGatewayCreationError = "Application007";
-        public const string ApplicationTypeMismatch = "Application008";
-        public const string ApplicationDuplicatedError = "Application.Duplicated";
+        public const string ApplicationNotFoundError = "ApplicationNotFoundError";
+        public const string ApplicationInIncorrectStateError = "ApplicationInIncorrectStateError";
+        public const string ApplicationViewIdNotFound = "ApplicationViewIdNotFound";
+        public const string ApplicationCreationError = "ApplicationCreationError";
+        public const string ApplicationGatewayCreationError = "ApplicationGatewayCreationError";
+        public const string ApplicationTypeMismatch = "ApplicationTypeMismatch";
+        public const string ApplicationDuplicatedError = "ApplicationDuplicatedError";
     }
 }
