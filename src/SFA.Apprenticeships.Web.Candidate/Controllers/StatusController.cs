538c3d29521cfd851f29d4f7417a93e5633dba1c
David Winchurch <david.winchurch@iterativeit.com>
2015-02-05 11:11:02 +0000

    Removed direct usage of repositories in Status Controller

--- src/SFA.Apprenticeships.Web.Candidate/Controllers/StatusController.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Controllers/StatusController.cs
@@ -3,32 +3,30 @@
     using System;
     using System.Threading.Tasks;
     using System.Web.Mvc;
-    using Application.Location;
-    using Domain.Interfaces.Repositories;
+    using Providers;
 
     public class StatusController : CandidateControllerBase
     {
-        //todo: none of these references are "allowed" - should be calling services
-        private readonly IApprenticeshipApplicationReadRepository _apprenticeshipApplicationReadRepository;
-        private readonly ICandidateReadRepository _candidateReadRepository;
-        private readonly ILocationLookupProvider _locationLookupProvider;
+        private readonly IApprenticeshipApplicationProvider _apprenticeshipApplicationProvider;
+        private readonly ICandidateServiceProvider _candidateServiceProvider;
+        private readonly ISearchProvider _searchProvider;
 
-        public StatusController(ILocationLookupProvider locationLookupProvider,
-            IApprenticeshipApplicationReadRepository apprenticeshipApplicationReadRepository,
-            ICandidateReadRepository candidateReadRepository)
+        public StatusController(ISearchProvider searchProvider,
+            IApprenticeshipApplicationProvider apprenticeshipApplicationProvider,
+            ICandidateServiceProvider candidateServiceProvider)
         {
-            _locationLookupProvider = locationLookupProvider;
-            _apprenticeshipApplicationReadRepository = apprenticeshipApplicationReadRepository;
-            _candidateReadRepository = candidateReadRepository;
+            _searchProvider = searchProvider;
+            _apprenticeshipApplicationProvider = apprenticeshipApplicationProvider;
+            _candidateServiceProvider = candidateServiceProvider;
         }
 
         public ActionResult Index()
         {
             var statusTasks = new[]
             {
-                new Task(() => _apprenticeshipApplicationReadRepository.Get(Guid.NewGuid())),
-                new Task(() => _candidateReadRepository.Get(Guid.NewGuid())),
-                new Task(() => _locationLookupProvider.FindLocation("London"))
+                new Task(() => _apprenticeshipApplicationProvider.GetApplicationViewModel(Guid.NewGuid(), 123456)),
+                new Task(() => _candidateServiceProvider.GetCandidate(Guid.NewGuid())),
+                new Task(() => _searchProvider.FindLocation("London"))
             };
 
             Parallel.ForEach(statusTasks, task => task.Start());
