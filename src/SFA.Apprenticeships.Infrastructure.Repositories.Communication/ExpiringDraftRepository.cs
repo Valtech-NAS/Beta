b99b31a5840d27268fb240e66321bd82b01621c5
David Winchurch <david.winchurch@iterativeit.com>
2015-02-05 09:46:37 +0000

    Added logging in ExpiringDraftRepository removing a TODO

--- src/SFA.Apprenticeships.Infrastructure.Repositories.Communication/ExpiringDraftRepository.cs
+++ src/SFA.Apprenticeships.Infrastructure.Repositories.Communication/ExpiringDraftRepository.cs
@@ -12,7 +12,6 @@
     using MongoDB.Bson;
     using MongoDB.Driver.Builders;
 
-    //todo: add logging (see other repo classes)
     public class ExpiringDraftRepository : CommunicationRepository<ExpiringDraft>, IExpiringDraftRepository
     {
         private readonly ILogService _logger;
@@ -27,25 +26,39 @@
 
         public void Save(ExpiringDraft expiringDraft)
         {
+            _logger.Debug("Calling repository to save expiring draft with Id={0}, CandidateId={1}, ApplicationId={2}", expiringDraft.EntityId, expiringDraft.CandidateId, expiringDraft.ApplicationId);
+            
             var mongoExpiringDraft = _mapper.Map<ExpiringDraft, MongoExpiringDraft>(expiringDraft);
             
             UpdateEntityTimestamps(mongoExpiringDraft);
             mongoExpiringDraft.SentDateTime = mongoExpiringDraft.BatchId.HasValue ? mongoExpiringDraft.DateUpdated : null;
 
+            _logger.Debug("Saved expiring draft to repository with Id={0}, CandidateId={1}, ApplicationId={2}", expiringDraft.EntityId, expiringDraft.CandidateId, expiringDraft.ApplicationId);
+
             Collection.Save(mongoExpiringDraft);
         }
-
+         
         public void Delete(ExpiringDraft expiringDraft)
         {
+            _logger.Debug("Calling repository to expiring draft with Id={0}", expiringDraft.EntityId);
+
             Collection.Remove(Query.EQ("_id", expiringDraft.EntityId));
+
+            _logger.Debug("Deleted expiring draft with Id={0}", expiringDraft.EntityId);
         }
 
         public Dictionary<Guid, List<ExpiringDraft>> GetCandidatesDailyDigest()
         {
+            _logger.Debug("Calling repository to get all candidates expiring drafts for their daily digest");
+
             var mongoExpiringDrafts = Collection.FindAs<MongoExpiringDraft>(Query.EQ("BatchId", BsonNull.Value));
             var expiringDrafts = _mapper.Map<IEnumerable<MongoExpiringDraft>, IEnumerable<ExpiringDraft>>(mongoExpiringDrafts);
 
-            return expiringDrafts.GroupBy(x => x.CandidateId).ToDictionary(grp => grp.Key, grp => grp.ToList());
+            var candidatesDailyDigest = expiringDrafts.GroupBy(x => x.CandidateId).ToDictionary(grp => grp.Key, grp => grp.ToList());
+
+            _logger.Debug("Found expiring drafts for {0} candidates", candidatesDailyDigest.Count);
+
+            return candidatesDailyDigest;
         }
     }
 }
