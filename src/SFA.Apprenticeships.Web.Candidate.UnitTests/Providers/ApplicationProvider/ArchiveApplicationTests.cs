10df6d67c7f5c2e9e9c1bbc290b32b848d82c4dc
David Winchurch <david.winchurch@iterativeit.com>
2015-02-06 16:54:22 +0000

    More unit tests for providers

---
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Providers/ApplicationProvider/ArchiveApplicationTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Providers.ApplicationProvider
+{
+    using System;
+    using Common.Models.Application;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ArchiveApplicationTests : ApprenticeshipApplicationProviderTestsBase
+    {
+        [Test]
+        public void GivenException_ThenFailedApplicationViewModelIsReturned()
+        {
+            var candidateId = Guid.NewGuid();
+            CandidateService.Setup(cs => cs.ArchiveApplication(candidateId, ValidVacancyId)).Throws<Exception>();
+
+            var returnedViewModel = ApprenticeshipApplicationProvider.ArchiveApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeTrue();
+            returnedViewModel.ViewModelStatus.Should().Be(ApplicationViewModelStatus.Error);
+            returnedViewModel.ViewModelMessage.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void GivenSuccessfulArchive_ThenSuccessfulViewModelIsReturned()
+        {
+            var candidateId = Guid.NewGuid();
+            var returnedViewModel = ApprenticeshipApplicationProvider.ArchiveApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeFalse();
+            returnedViewModel.ViewModelStatus.Should().Be(ApplicationViewModelStatus.Ok);
+            returnedViewModel.ViewModelMessage.Should().BeNullOrEmpty();
+        }
+    }
+}
--- src/SFA.Apprenticeships.Web.Candidate.UnitTests/Providers/ApplicationProvider/SubmitApplicationTests.cs
+++ src/SFA.Apprenticeships.Web.Candidate.UnitTests/Providers/ApplicationProvider/SubmitApplicationTests.cs
@@ -1,9 +1,13 @@
 ﻿namespace SFA.Apprenticeships.Web.Candidate.UnitTests.Providers.ApplicationProvider
 {
     using System;
-    using Builders;
+    using Application.Interfaces.Applications;
+    using Candidate.ViewModels.VacancySearch;
+    using Common.Models.Application;
     using Constants.Pages;
     using Domain.Entities.Applications;
+    using Domain.Entities.Exceptions;
+    using FluentAssertions;
     using NUnit.Framework;
 
     [TestFixture]
@@ -13,11 +17,66 @@
         public void GivenViewModelHasError_ThenItIsReturned()
         {
             var candidateId = Guid.NewGuid();
-            var viewModel = new ApprenticeshipApplicationViewModelBuilder()
-                .HasError(ApplicationStatuses.ExpiredOrWithdrawn, MyApplicationsPageMessages.ApprenticeshipNoLongerAvailable)
-                .Build();
+            CandidateService.Setup(cs => cs.CreateApplication(candidateId, ValidVacancyId)).Returns((ApprenticeshipApplicationDetail) null);
 
             var returnedViewModel = ApprenticeshipApplicationProvider.SubmitApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeTrue();
+            returnedViewModel.Status.Should().Be(ApplicationStatuses.ExpiredOrWithdrawn);
+            returnedViewModel.ViewModelMessage.Should().Be(MyApplicationsPageMessages.ApprenticeshipNoLongerAvailable);
+        }
+
+        [Test]
+        public void GivenApplicationIsInCorrectState_ThenModelIsReturnedWithThatState()
+        {
+            var candidateId = Guid.NewGuid();
+            ApprenticeshipVacancyDetailProvider.Setup(p => p.GetVacancyDetailViewModel(candidateId, ValidVacancyId)).Returns(new VacancyDetailViewModel());
+            CandidateService.Setup(cs => cs.CreateApplication(candidateId, ValidVacancyId)).Returns(new ApprenticeshipApplicationDetail());
+            CandidateService.Setup(cs => cs.SubmitApplication(candidateId, ValidVacancyId)).Throws(new CustomException(ErrorCodes.ApplicationInIncorrectStateError));
+
+            var returnedViewModel = ApprenticeshipApplicationProvider.SubmitApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeFalse();
+            returnedViewModel.ViewModelStatus.Should().Be(ApplicationViewModelStatus.ApplicationInIncorrectState);
+        }
+
+        [Test]
+        public void GivenCustomException_ThenFailedApplicationViewModelIsReturned()
+        {
+            var candidateId = Guid.NewGuid();
+            ApprenticeshipVacancyDetailProvider.Setup(p => p.GetVacancyDetailViewModel(candidateId, ValidVacancyId)).Returns(new VacancyDetailViewModel());
+            CandidateService.Setup(cs => cs.CreateApplication(candidateId, ValidVacancyId)).Returns(new ApprenticeshipApplicationDetail());
+            CandidateService.Setup(cs => cs.SubmitApplication(candidateId, ValidVacancyId)).Throws(new CustomException(ErrorCodes.ApplicationGatewayCreationError));
+
+            var returnedViewModel = ApprenticeshipApplicationProvider.SubmitApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeTrue();
+            returnedViewModel.ViewModelStatus.Should().Be(ApplicationViewModelStatus.Error);
+            returnedViewModel.ViewModelMessage.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void GivenException_ThenFailedApplicationViewModelIsReturned()
+        {
+            var candidateId = Guid.NewGuid();
+            ApprenticeshipVacancyDetailProvider.Setup(p => p.GetVacancyDetailViewModel(candidateId, ValidVacancyId)).Returns(new VacancyDetailViewModel());
+            CandidateService.Setup(cs => cs.CreateApplication(candidateId, ValidVacancyId)).Returns(new ApprenticeshipApplicationDetail());
+            CandidateService.Setup(cs => cs.SubmitApplication(candidateId, ValidVacancyId)).Throws<Exception>();
+
+            var returnedViewModel = ApprenticeshipApplicationProvider.SubmitApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeTrue();
+            returnedViewModel.ViewModelStatus.Should().Be(ApplicationViewModelStatus.Error);
+            returnedViewModel.ViewModelMessage.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void GivenSuccessfulSubmission_ThenSuccessfulViewModelIsReturned()
+        {
+            var candidateId = Guid.NewGuid();
+            ApprenticeshipVacancyDetailProvider.Setup(p => p.GetVacancyDetailViewModel(candidateId, ValidVacancyId)).Returns(new VacancyDetailViewModel());
+            CandidateService.Setup(cs => cs.CreateApplication(candidateId, ValidVacancyId)).Returns(new ApprenticeshipApplicationDetail());
+
+            var returnedViewModel = ApprenticeshipApplicationProvider.SubmitApplication(candidateId, ValidVacancyId);
+            returnedViewModel.HasError().Should().BeFalse();
+            returnedViewModel.ViewModelStatus.Should().Be(ApplicationViewModelStatus.Ok);
+            returnedViewModel.ViewModelMessage.Should().BeNullOrEmpty();
         }
     }
 }
