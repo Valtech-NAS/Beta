2893134d5895875572fc2529d446b507dbdf4199
David Winchurch <david.winchurch@iterativeit.com>
2015-02-12 16:12:30 +0000

    Updated content for account unlock

--- src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/AccountUnlockPageMessages.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Constants/Pages/AccountUnlockPageMessages.cs
@@ -6,9 +6,9 @@
     {
         public const string AccountUnlockFailed = "There’s been an error unlocking your account. Please try again.";
         public const string AccountUnlockCodeExpired = "The code to unlock your account has expired. Please use the new one we’ve sent you.";
-        public const string WrongEmailAddressOrAccountUnlockCodeErrorText = "Please enter a valid email address or unlock account code";
+        public const string WrongEmailAddressOrAccountUnlockCodeErrorText = "You may have entered an invalid email address or unlock code.  Please try again.";
         public const string AccountUnlockedText = "You've successfully unlocked your account";
-        public const string AccountUnlockCodeMayHaveBeenResent = "Please check your email for an account unlock code. If you do not receive one, please check you have entered your email correctly and that your account is locked.";
+        public const string AccountUnlockCodeMayHaveBeenResent = "We have emailed you an unlock account code.  If you don't receive it you may have entered an incorrect email address or your account may not be locked.";
         public const string AccountUnlockResendCodeFailed = "There’s been a problem sending you a code to unlock your account. Please try again.";
     }
 }
