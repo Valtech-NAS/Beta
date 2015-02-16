119f0d93efd7f0eb8ed6ac9fce0ee896da96ebed
charge-valtech <henry.charge@valtech.co.uk>
2015-02-16 10:32:55 +0000

    Fix: Use FastClick on mobile only, certain Anti virus products are blocking fastclick based on its name

--- src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/scripts.js
+++ src/SFA.Apprenticeships.Web.Candidate/Content/_assets/js/scripts.js
@@ -1,5 +1,28 @@
 $(function() {
-  FastClick.attach(document.body);
+    var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    if (isMobile.any()) {
+        FastClick.attach(document.body);
+    }
 
   function isAndroid() {
     var nua = navigator.userAgent,
