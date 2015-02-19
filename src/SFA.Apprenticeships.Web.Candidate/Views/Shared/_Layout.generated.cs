8ecb67fe3de594cbacb0881cf3ad52dceb7a2dd3
charge-valtech <henry.charge@valtech.co.uk>
2015-02-19 16:24:07 +0000

    Update CSS version numbers

--- src/SFA.Apprenticeships.Web.Candidate/Views/Shared/_Layout.generated.cs
+++ src/SFA.Apprenticeships.Web.Candidate/Views/Shared/_Layout.generated.cs
@@ -148,7 +148,7 @@ WriteLiteral("\r\n    <!--[if gt IE 8]><!-->");
 
             
             #line 22 "..\..\Views\Shared\_Layout.cshtml"
-                     Write(Styles.Render(Url.CdnContent("css/main-1.5.0.862.css", "~/Content/_assets")));
+                     Write(Styles.Render(Url.CdnContent("css/main-1.5.0.1098.css", "~/Content/_assets")));
 
             
             #line default
@@ -157,7 +157,7 @@ WriteLiteral("<!--<![endif]-->\r\n    <!--[if lte IE 8]>");
 
             
             #line 23 "..\..\Views\Shared\_Layout.cshtml"
-                 Write(Styles.Render(Url.CdnContent("css/main-ie8-1.5.0.862.css", "~/Content/_assets")));
+                 Write(Styles.Render(Url.CdnContent("css/main-ie8-1.5.0.1098.css", "~/Content/_assets")));
 
             
             #line default
